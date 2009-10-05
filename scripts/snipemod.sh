289308b12be072e5bab9a96300ab0731ba026fea
Austin Trask <austin.trask@gmail.com>
2009-10-05 03:15:29 +0000

    added touch and sleep to quarantine scripts
    Autoconverted from SVN (revision:26)

--- scripts/snipemod.sh
+++ scripts/snipemod.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-find ~/quarantine/* -type d -amin -1 -print| while read FILE
-do	
-	chmod -R 000 $FILE
-done
-
-find ~/quarantine/* -type f -amin -1 -print| while read FILE
-do	
-	chmod -R 000 $FILE
-done
+touch -a $1
+chmod -R 000 $1
