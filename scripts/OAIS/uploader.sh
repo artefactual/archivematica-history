798942610f8d9d95aae9f44e90525321fd554c10
Austin Trask <austin.trask@gmail.com>
2009-10-08 23:51:17 +0000

    added retry to qubit uploader
    Autoconverted from SVN (revision:29)

--- scripts/OAIS/uploader.sh
+++ scripts/OAIS/uploader.sh
@@ -2,6 +2,6 @@
 
 cp -a $2 ~/archivalstorage/.
 
-curl -c /tmp/qubitcookies.txt -d "login%5Bemail%5D=demo%40example.com&login%5Bpassword%5D=demo&referer=&commit=log+in" http://localhost/index.php/user/login
-curl -v -b /tmp/qubitcookies.txt -F identifier=$1 -F title=$1 -F upload_file[140]=@$2 -F commit=create  http://localhost/index.php/informationobject/create/isad
+curl --retry 20 -c /tmp/qubitcookies.txt -d "login%5Bemail%5D=demo%40example.com&login%5Bpassword%5D=demo&referer=&commit=log+in" http://localhost/index.php/user/login
+curl -v --retry 20 -b /tmp/qubitcookies.txt -F identifier=$1 -F title=$1 -F upload_file[140]=@$2 -F commit=create  http://localhost/index.php/informationobject/create/isad
 
