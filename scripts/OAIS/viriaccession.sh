b05b55a4d54b7d37e57adc2bb25a9397aecc07ad
Austin Trask <austin.trask@gmail.com>
2009-10-14 23:56:39 +0000

    fixed issue 51 added -maxdepth and -perm
    Autoconverted from SVN (revision:30)

--- scripts/OAIS/viriaccession.sh
+++ scripts/OAIS/viriaccession.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find ~/quarantine/* -amin +2 -print| while read FILE
+find ~/quarantine/* -amin +2 -maxdepth 0 -perm 0000 -print| while read FILE
 do
 	if [ -d "$FILE" ]; then
 		chmod 700 $FILE		
@@ -8,9 +8,10 @@ do
 		mkdir /home/demo/accessionrecords/$NOW		
 		mkdir -p /tmp/accession-$NOW
 		mv $FILE /tmp/accession-$NOW
-		chmod -R 700 /tmp/accession-$NOW/*
+		chmod 700 /tmp/accession-$NOW/$FILE
 		find /tmp/accession-$NOW/ -type f -print| while read NEWDOCS
 			do
+				chmod 700 $NEWDOCS
 				clamscan --move=/home/demo/possiblevirii/  $NEWDOCS >> ~/accessionrecords/$NOW/virus.log
 				/usr/local/OAIS/fits/folderaccess.sh  $NEWDOCS $NOW 
 				echo "Acession of $NEWDOCS completed successfully" >> ~/accessionrecords/accession.log
@@ -21,7 +22,7 @@ do
 		chmod 700 $FILE		
 		NOW=`date +%s`
 		mkdir /home/demo/accessionrecords/$NOW
-		clamscan --move=/home/demo/possiblevirii/  $FILE >> ~/accessionrecords/$NOW/virus.log
+		clamscan --move=/home/demo/possiblevirii/  $FILE >> ~/accessionrecords/$NOW/virus.log		
 		/usr/local/OAIS/fits/access.sh $FILE $NOW		
 		mv $FILE ~/normalizeMe/.
 		echo "Acession of $FILE completed successfully" >> ~/accessionrecords/accession.log
