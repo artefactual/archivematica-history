a12b3f6d267cbb3e842d57f3b136cc33a970aeb5
Jack Bates <jack@nottheoilrig.com>
2009-11-21 23:57:07 +0000

    Drop some unneeded parameters
    Autoconverted from SVN (revision:43)

--- scripts/OAIS/uploader.sh
+++ scripts/OAIS/uploader.sh
@@ -2,4 +2,4 @@
 
 cp -a $2 ~/storeAIP/.
 
-curl --retry 20 -c - -d "login%5Bemail%5D=demo%40example.com&login%5Bpassword%5D=demo&referer=&commit=log+in" -o /dev/null http://localhost/index.php/user/login | curl -v --retry 20 -b - -F title=$1 -F upload_file[140]=@$2 -F commit=create  http://localhost/index.php/informationobject/create/isad
+curl --retry 20 -c - -d "login%5Bemail%5D=demo%40example.com&login%5Bpassword%5D=demo" -o /dev/null http://localhost/index.php/user/login | curl -v --retry 20 -b - -F title=$1 -F upload_file[140]=@$2 http://localhost/index.php/informationobject/create/isad
