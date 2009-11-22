2a017626052999d1529c444ebfa4537d22a7b340
Jack Bates <jack@nottheoilrig.com>
2009-11-22 00:34:04 +0000

    Oops, fix mistake in commit 45
    Autoconverted from SVN (revision:47)

--- scripts/OAIS/uploader.sh
+++ scripts/OAIS/uploader.sh
@@ -2,4 +2,4 @@
 
 cp -a $2 ~/storeAIP/.
 
-curl --retry 20 -c - -F email=demo@example.com -F password=demo -o /dev/null http://localhost/index.php/user/login | curl -v --retry 20 -b - -F title=$1 -F upload_file[140]=@$2 http://localhost/index.php/informationobject/create/isad
+curl --retry 20 -c - -F login[email]=demo@example.com -F login[password]=demo -o /dev/null http://localhost/index.php/user/login | curl -v --retry 20 -b - -F title=$1 -F upload_file[140]=@$2 http://localhost/index.php/informationobject/create/isad
