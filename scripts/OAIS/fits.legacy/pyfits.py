d1be5115de1d0f9ceded414695c65b071fbdc9b5
Austin Trask <austin.trask@gmail.com>
2009-11-25 04:47:30 +0000

    updated to fits v 2.6
    Autoconverted from SVN (revision:51)

--- scripts/OAIS/fits.legacy/pyfits.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import os
-import sys
-
-fitsinput = sys.argv[1]
-fitsoutput = sys.argv[2]
-
-os.system("cd  /usr/local/OAIS/fits; ./fits.sh -i " + fitsinput + " -o " + fitsoutput)
