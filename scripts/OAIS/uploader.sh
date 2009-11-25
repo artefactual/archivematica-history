feeb829f2fb149ed5ed746cdf4cb7550acdab5e6
Austin Trask <austin.trask@gmail.com>
2009-11-25 04:33:44 +0000

    moved fits to fits.legacy
    Autoconverted from SVN (revision:49)

--- scripts/OAIS/uploader.sh
+++ scripts/OAIS/uploader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -a $2 ~/storeAIP/.
+#cp -a $2 ~/storeAIP/.
 rm /tmp/foobject
 curl --retry 20 -s -c /tmp/qubitcookies -F login[email]=demo@example.com -F login[password]=demo http://localhost/index.php/user/login 
 curl --retry 20 -s -b /tmp/qubitcookies -d "parent=%2Findex.php%2Finformationobject%2Fshow%2Fisad%2F1&title=$1" --write-out "%{redirect_url}" http://localhost/index.php/informationobject/create/isad > /tmp/foobject
