d1cc9713c5e5e8b5174cd6dd25bbb6072ab1230a
Austin Trask <austin.trask@gmail.com>
2010-01-18 22:29:40 +0000

    changed viriaccession script variable from epoch-date to UUID
    Autoconverted from SVN (revision:82)

--- includes/archivematica/viriaccession.sh
+++ includes/archivematica/viriaccession.sh
@@ -4,26 +4,26 @@ find ~/quarantine/* -maxdepth 0 -amin +2 -perm 0000 -print| while read FILE
 do
 	if [ -d "$FILE" ]; then
 		chmod 700 $FILE		
-		NOW=`date +%s`
-		mkdir /home/demo/accessionreports/$NOW		
-		mkdir -p /tmp/accession-$NOW
-		mv $FILE /tmp/accession-$NOW
-		chmod 700 /tmp/accession-$NOW/*
-		find /tmp/accession-$NOW/ -type f -print| while read NEWDOCS
+		UUID=`uuid`
+		mkdir /home/demo/accessionreports/$UUID		
+		mkdir -p /tmp/accession-$UUID
+		mv $FILE /tmp/accession-$UUID
+		chmod 700 /tmp/accession-$UUID/*
+		find /tmp/accession-$UUID/ -type f -print| while read NEWDOCS
 			do
 				chmod 700 $NEWDOCS
 				clamscan --move=/home/demo/possiblevirii/  $NEWDOCS >> ~/accessionreports/virus.log
-				/opt/externals/fits/folderaccess.sh  $NEWDOCS $NOW 
+				/opt/externals/fits/folderaccess.sh  $NEWDOCS $UUID 
 				echo "Acession of $NEWDOCS completed successfully" >> ~/accessionreports/accession.log
 			done		
-		mv /tmp/accession-$NOW/* ~/prepareAIP/.
-		rm -rf /tmp/accession-$NOW
+		mv /tmp/accession-$UUID/* ~/prepareAIP/.
+		rm -rf /tmp/accession-$UUID
 	elif [ -f "$FILE" ]; then
 		chmod 700 $FILE		
-		NOW=`date +%s`
-		mkdir /home/demo/accessionreports/$NOW
+		UUID=`uuid`
+		mkdir /home/demo/accessionreports/$UUID
 		clamscan --move=/home/demo/possiblevirii/  $FILE >> ~/accessionreports/virus.log		
-		/opt/externals/fits/access.sh $FILE $NOW		
+		/opt/externals/fits/access.sh $FILE $UUID		
 		mv $FILE ~/prepareAIP/.
 		echo "Accession of $FILE completed successfully" >> ~/accessionreports/accession.log
 	else
