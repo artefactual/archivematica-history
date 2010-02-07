362b8e4a3732e7e1bbdeddc85df0726092151640
Austin Trask <austin.trask@gmail.com>
2010-02-07 23:22:36 +0000

    remove old fits
    Autoconverted from SVN (revision:102)

--- includes/externals/fits/access.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NOW=`date +%s`
-shortfile=`basename "$1"`
-
-if [ -e $1 ];then
-  cd /opt/externals/fits/ ; ./fits.sh -i $1 -o /home/demo/accessionreports/$2/$shortfile.log
-else
-  echo "$1 was had by the viri checker"
-fi
