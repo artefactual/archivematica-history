bb28dabf40ccde82c713b87ad285b395fae07c0e
Peter Van Garderen <vangarderen.peter@gmail.com>
2010-02-20 23:00:22 +0000

    style tables and headers
    Autoconverted from SVN (revision:134)

--- includes/archivematica/dashboard/apps/dashboard/modules/aip/templates/indexSuccess.php
+++ includes/archivematica/dashboard/apps/dashboard/modules/aip/templates/indexSuccess.php
@@ -3,25 +3,19 @@
 <table>
   <thead>
     <tr>
-      <th>Id</th>
       <th>Aip status</th>
       <th>Identifier</th>
       <th>Date accepted</th>
-      <th>Checksum</th>
-      <th>Checksum type</th>
       <th>Location</th>
       <th>Dip location</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($aips as $aip): ?>
-    <tr>
-      <td><a href="<?php echo url_for('aip/show?id='.$aip->getId()) ?>"><?php echo $aip->getId() ?></a></td>
-      <td><?php echo $aip->getAipStatusId() ?></td>
-      <td><?php echo $aip->getIdentifier() ?></td>
+    <tr class="<?php echo 0 == $row++ % 2 ? 'even' : 'odd' ?>">
+      <td><?php echo $aip->getAipStatus() ?></td>
+      <td><a href="<?php echo url_for('aip/show?id='.$aip->getId()) ?>"><?php echo $aip->getIdentifier() ?></a></td>
       <td><?php echo $aip->getDateAccepted() ?></td>
-      <td><?php echo $aip->getChecksum() ?></td>
-      <td><?php echo $aip->getChecksumType() ?></td>
       <td><?php echo $aip->getLocation() ?></td>
       <td><?php echo $aip->getDipLocation() ?></td>
     </tr>
--- includes/archivematica/dashboard/apps/dashboard/modules/sip/templates/indexSuccess.php
+++ includes/archivematica/dashboard/apps/dashboard/modules/sip/templates/indexSuccess.php
@@ -3,29 +3,21 @@
 <table>
   <thead>
     <tr>
-      <th>Id</th>
-      <th>Sip status</th>
+      <th>Status</th>
       <th>Identifier</th>
       <th>Title</th>
       <th>Date submitted</th>
       <th>Provenance</th>
-      <th>Part of</th>
-      <th>Checksum</th>
-      <th>Checksum type</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($sips as $sip): ?>
-    <tr>
-      <td><a href="<?php echo url_for('sip/show?id='.$sip->getId()) ?>"><?php echo $sip->getId() ?></a></td>
-      <td><?php echo $sip->getSipStatusId() ?></td>
-      <td><?php echo $sip->getIdentifier() ?></td>
+    <tr class="<?php echo 0 == $row++ % 2 ? 'even' : 'odd' ?>">
+      <td><?php echo $sip->getSipStatus() ?></td>
+      <td><a href="<?php echo url_for('sip/show?id='.$sip->getId()) ?>"><?php echo $sip->getIdentifier() ?></a></td>
       <td><?php echo $sip->getTitle() ?></td>
       <td><?php echo $sip->getDateSubmitted() ?></td>
       <td><?php echo $sip->getProvenance() ?></td>
-      <td><?php echo $sip->getPartOf() ?></td>
-      <td><?php echo $sip->getChecksum() ?></td>
-      <td><?php echo $sip->getChecksumType() ?></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
