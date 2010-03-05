93815aa959b9fd1bd7a973e71efa3875c1d4d5c0
Peter Van Garderen <vangarderen.peter@gmail.com>
2010-03-05 16:48:07 +0000

    Dashboard: move SIP status to a partial
    Autoconverted from SVN (revision:171)

---
+++ includes/archivematica/dashboard/apps/dashboard/modules/sip/templates/_status.php
@@ -0,0 +1,17 @@
+<h2>Status log</h2>
+<table>
+  <tbody>
+  <thead>
+    <tr>
+      <th><?php echo __('SIP status')?></th>
+      <th><?php echo __('Closed at') ?></th>
+      <th><?php echo __('Opened at') ?></th>
+    </tr>
+  </thead>
+    <tr class="odd"><td><?php echo $sip->getCurrentStatusLog()->getSipStatus()->getName() ?></td><td></td><td><?php echo $sip->getCurrentStatusLog()->getOpenedAt() ?></td></tr>
+  <?php foreach($sip->getStatusLog() as $log): ?>
+    <tr class="<?php echo 0 == $row++ % 2 ? 'even' : 'odd' ?>">
+    <td><?php echo $log->getSipStatus()?></td><td><?php echo $log->getClosedAt() ?></td><td><?php echo $log->getOpenedAt() ?></td></tr>
+  <?php endforeach; ?>
+  </tbody>
+</table>
--- includes/archivematica/dashboard/apps/dashboard/modules/sip/templates/showSuccess.php
+++ includes/archivematica/dashboard/apps/dashboard/modules/sip/templates/showSuccess.php
@@ -24,36 +24,14 @@
       <th>Part of:</th>
       <td><?php echo $sip->getPartOf() ?></td>
     </tr>
-    <tr>
-      <th>Checksum:</th>
-      <td><?php echo $sip->getChecksum() ?></td>
-    </tr>
-    <tr>
-      <th>Checksum type:</th>
-      <td><?php echo $sip->getChecksumType() ?></td>
-    </tr>
   </tbody>
 </table>
 
 <hr />
 
-<h2>Status log</h2>
-<table>
-  <tbody>
-  <thead>
-    <tr>
-      <th><?php echo __('SIP status')?></th>
-      <th><?php echo __('Closed at') ?></th>
-      <th><?php echo __('Opened at') ?></th>
-    </tr>
-  </thead>
-    <tr class="odd"><td><?php echo $sip->getCurrentStatusLog()->getSipStatus()->getName() ?></td><td></td><td><?php echo $sip->getCurrentStatusLog()->getOpenedAt() ?></td></tr>
-  <?php foreach($sip->getStatusLog() as $log): ?>
-    <tr class="<?php echo 0 == $row++ % 2 ? 'even' : 'odd' ?>">
-    <td><?php echo $log->getSipStatus()?></td><td><?php echo $log->getClosedAt() ?></td><td><?php echo $log->getOpenedAt() ?></td></tr>
-  <?php endforeach; ?>
-  </tbody>
-</table>
+<?php echo get_partial('sip/status', array('sip' => $sip)) ?>
+
+<hr />
 
 <a href="<?php echo url_for('sip/edit?id='.$sip->getId()) ?>">Edit</a>
 &nbsp;
