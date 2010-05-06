2d7ffd5fb0804c0e9f8cc32d245eb20966cdc84a
Austin Trask <austin.trask@gmail.com>
2010-05-06 19:31:21 +0000

    fixed processSIP.sh formating
    Autoconverted from SVN (revision:259)

--- includes/archivematica/processSIP.sh
+++ includes/archivematica/processSIP.sh
@@ -23,48 +23,46 @@
 
 find ~/3-quarantineSIP/* -maxdepth 0 -amin +1 -perm 0000 -print| while read FILE
 do
-	if [ -d "$FILE" ]; then
-		chmod 700 "$FILE"		
-		UUID=`uuid`
-		mkdir /home/demo/ingestLogs/$UUID
-		mkdir /tmp/$UUID		
-		mv "$FILE" /tmp/$UUID/.
-		chmod 700 /tmp/$UUID/*
-		if [ ! -f /tmp/$UUID/SIP.xml ]
-		then
-			tmpDir=`pwd`
-			cd /tmp/$UUID/
-			/opt/archivematica/SIPxmlModifiers/CreateSipAndAddDublinCoreStructure.py
-			cd $tmpDir
-			
-		fi
-		mv /tmp/$UUID/SIP.xml /home/demo/ingestLogs/$UUID/SIP.xml
-		
-		#extract all of the .zip .rar etc.
-		python /opt/externals/easy-extract/easy_extract.py /tmp/$UUID/ -w -f -r -n 2>&1 >> /home/demo/ingestLogs/$UUID/extraction.log
-		/opt/archivematica/SIPxmlModifiers/addFileStructureToSIP.py "/home/demo/ingestLogs/$UUID" $UUID
-		/opt/archivematica/SIPxmlModifiers/addUUIDasDCidentifier.py "/home/demo/ingestLogs/$UUID" $UUID
-		detox -rv /tmp/$UUID >> /home/demo/ingestLogs/$UUID/detox.log
-		cleanName=`ls /tmp/$UUID`
-		python /opt/archivematica/SIPxmlModifiers/addDetoxLogToSIP.py "/home/demo/ingestLogs/$UUID" "$FILE"
-		find /tmp/$UUID/ -type f -print| while read NEWDOCS
-			do
-				# XENADIR=`dirname "$NEWDOCS"`  # needed if using normalize.sh				
-				chmod 700 $NEWDOCS
-				clamscan --move=/home/demo/SIPerrors/possibleVirii/  $NEWDOCS >> ~/ingestLogs/$UUID/virusSCAN.log
-				/opt/archivematica/folderaccess.sh  $NEWDOCS $UUID  # run  FITS
-				echo "Receipt of $NEWDOCS completed" >> ~/ingestLogs/accession.log
-				# /opt/archivematica/normalize.py $NEWDOCS $UUID # move to 5-prepareAIP
-			done
-		cp -a /home/demo/ingestLogs/$UUID /tmp/$UUID/$cleanName/ingestLogs		
-		mv /tmp/$UUID/$cleanName /home/demo/4-appraiseSIP/$cleanName-$UUID
-		DISPLAY=:0.0 /usr/bin/notify-send "ingest" "$cleanName ready for appraisal"
-		rm -rf /tmp/$UUID
-	elif [ -f "$FILE" ]; then
-		chmod 700 "$FILE"
-		mv "$FILE" /home/demo/SIPerrors/.
-		DISPLAY=:0.0 /usr/bin/notify-send "SIP Error" "A SIP must be submitted as a directory"
-	else
-		echo "$FILE is not a file or directory"
-	fi
+  if [ -d "$FILE" ]; then
+    chmod 700 "$FILE"		
+    UUID=`uuid`
+    mkdir /home/demo/ingestLogs/$UUID
+    mkdir /tmp/$UUID		
+    mv "$FILE" /tmp/$UUID/.
+    chmod 700 /tmp/$UUID/*
+    if [ ! -f /tmp/$UUID/SIP.xml ]
+    then
+      tmpDir=`pwd`
+      cd /tmp/$UUID/
+      /opt/archivematica/SIPxmlModifiers/CreateSipAndAddDublinCoreStructure.py
+      cd $tmpDir	
+    fi
+    mv /tmp/$UUID/SIP.xml /home/demo/ingestLogs/$UUID/SIP.xml
+
+    #extract all of the .zip .rar etc.
+    python /opt/externals/easy-extract/easy_extract.py /tmp/$UUID/ -w -f -r -n 2>&1 >> /home/demo/ingestLogs/$UUID/extraction.log
+    /opt/archivematica/SIPxmlModifiers/addFileStructureToSIP.py "/home/demo/ingestLogs/$UUID" $UUID
+    /opt/archivematica/SIPxmlModifiers/addUUIDasDCidentifier.py "/home/demo/ingestLogs/$UUID" $UUID
+    detox -rv /tmp/$UUID >> /home/demo/ingestLogs/$UUID/detox.log
+    cleanName=`ls /tmp/$UUID`
+    python /opt/archivematica/SIPxmlModifiers/addDetoxLogToSIP.py "/home/demo/ingestLogs/$UUID" "$FILE"
+    find /tmp/$UUID/ -type f -print| while read NEWDOCS
+      do
+        # XENADIR=`dirname "$NEWDOCS"`  # needed if using normalize.sh				
+        chmod 700 $NEWDOCS
+        clamscan --move=/home/demo/SIPerrors/possibleVirii/  $NEWDOCS >> ~/ingestLogs/$UUID/virusSCAN.log
+        /opt/archivematica/folderaccess.sh  $NEWDOCS $UUID  # run  FITS
+        echo "Receipt of $NEWDOCS completed" >> ~/ingestLogs/accession.log
+      done
+    cp -a /home/demo/ingestLogs/$UUID /tmp/$UUID/$cleanName/ingestLogs		
+    mv /tmp/$UUID/$cleanName /home/demo/4-appraiseSIP/$cleanName-$UUID
+    DISPLAY=:0.0 /usr/bin/notify-send "ingest" "$cleanName ready for appraisal"
+    rm -rf /tmp/$UUID
+  elif [ -f "$FILE" ]; then
+    chmod 700 "$FILE"
+    mv "$FILE" /home/demo/SIPerrors/.
+    DISPLAY=:0.0 /usr/bin/notify-send "SIP Error" "A SIP must be submitted as a directory"
+  else
+    echo "$FILE is not a file or directory"
+  fi
 done
