0aa9c340506b00170c928b41cea963f816abc509
Jesús García Crespo <jesus@sevein.com>
2010-05-08 19:32:08 +0000

    Update upload python script for new ICA-AtoM 1.0.9: - Add support for new ICA-AtoM URL schema - Update POST parameters for login action - Update search_error regular expression to catch login errors in new login template - Add support for upload external objects (--url option) - Add some debug information: symfony session, information object id, ... - Avoid calling register_openers() in favour of one unique opener with auth support - Cosmetic changes.
    Autoconverted from SVN (revision:266)

--- includes/archivematica/upload/upload-qubit.py
+++ includes/archivematica/upload/upload-qubit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # This file is part of Archivematica.
 #
@@ -23,8 +24,7 @@
 # @version svn: $Id$
 
 
-# -*- coding: utf-8 -*-
-
+import cookielib
 import optparse
 import os.path
 import re
@@ -34,14 +34,16 @@ import urllib2
 import urlparse
 
 from poster.encode import multipart_encode
-from poster.streaminghttp import register_openers
+from poster.streaminghttp import StreamingHTTPHandler, StreamingHTTPRedirectHandler, StreamingHTTPSHandler
 
-# Take into account that the url paths below changed in 1.1
-# This is compatible with 1.0.8.1
-URL_LOGIN='http://localhost/index.php/user/login'
-URL_CREATE_ISAD='http://localhost/index.php/informationobject/create/isad'
-URL_CREATE_DO='http://localhost/index.php/digitalobject/create'
-ROOT_ID='/index.php/informationobject/show/isad/1'
+# This is for ICA-AtoM 1.0.9 URL schema
+URL_BASE = 'http://localhost/index.php'
+URL_LOGIN = URL_BASE + '/;user/login'
+URL_CREATE_ISAD = URL_BASE + '/;create/isad'
+URL_CREATE_DO = URL_BASE + '/;digitalobject/create'
+
+# URL representation of QubitInformationObject root
+ROOT_ID = '/1;isad'
 
 class LoginError(Exception):
   response = None
@@ -53,15 +55,18 @@ class LoginError(Exception):
     Exception.__init__(self, self.message)
 
   def search_error(self):
-    regex = re.compile(r'<div class=\"form_error\">(.*?)</div>', re.DOTALL)
+    regex = re.compile(r'<div class=\"messages error\">\s+<ul>\s+<li>(.*?)</li>\s+</ul>\s+</div>', re.DOTALL)
     robj = regex.search(self.response)
 
     if robj is None:
-      return "Login was not successful"
+      return 'Login was not successful'
     else:
-      return robj.group(1).strip()
+      return robj.group(1).strip() + ' (this message was captured from server response)'
 
 class FileNotFound(Exception):
+  pass
+
+class URLNotAccessible(Exception):
   pass
 
 def get_id_from_url(url):
@@ -70,20 +75,34 @@ def get_id_from_url(url):
 
 def upload(opts):
   # Build opener with HTTPCookieProcessor, which send cookies back automatically
-  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+  cj = cookielib.CookieJar()
+  handlers = [StreamingHTTPHandler, StreamingHTTPRedirectHandler, StreamingHTTPSHandler]
+  opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj), *handlers)
   urllib2.install_opener(opener)
 
   # Login
-  data = { 'login[email]' : opts['email'], 'login[password]' : opts['password'] }
+  data = { 'email' : opts['email'], 'password' : opts['password'] }
   response = urllib2.urlopen(URL_LOGIN, urllib.urlencode(data))
 
+  # Print symfony session id
+  if opts['debug']:
+    for index, cookie in enumerate(cj):
+      if cookie.name == 'symfony':
+        print 'Symfony session ID: ' + cookie.value
+        break;
+
   # Check if login was successful
-  if 'user/login' in response.geturl():
+  if 'user/login' in response.url:
     raise LoginError(response)
 
   # Check if file exists
-  if os.path.exists(opts['file']) is False:
-    raise FileNotFound('File \'%s\' not found' % opts['file'])
+  if opts['file'] and os.path.exists(opts['file']) is False:
+    raise FileNotFound('File "%s" not found' % opts['file'])
+  elif opts['url']:
+    try:
+      urllib2.urlopen(opts['url'])
+    except:
+      raise URLNotAccessible('URL "%s" is not accessible' % opts['url'])
 
   # Create information object
   data = { 'title' : opts['title'], 'parent' : ROOT_ID }
@@ -92,11 +111,22 @@ def upload(opts):
   # Get information object id
   id = get_id_from_url(response.geturl())
 
-  # Upload digital object, (poster: register_openers(), multipart_encode())
-  register_openers()
-  data, headers = multipart_encode({ 'file' : open(opts['file']), 'informationObject' : id })
-  request = urllib2.Request(URL_CREATE_DO, data, headers)
-  response = urllib2.urlopen(request)
+  # Print information object id
+  if opts['debug']:
+    print 'New information object ID: ' + id
+
+  # Fetch file content or external resource
+  if opts['file']:
+    data, headers = multipart_encode({ 'file' : open(opts['file']), 'informationObject' : id })
+    request = urllib2.Request(URL_CREATE_DO, data, headers)
+    response = urllib2.urlopen(request)
+
+  elif opts['url']:
+    data = { 'url' : opts['url'], 'informationObject' : id }
+    response = urllib2.urlopen(URL_CREATE_DO, urllib.urlencode(data))
+
+  if opts['debug']:
+    print 'New information object URL: ' + response.url
 
 # Main program
 if __name__ == '__main__':
@@ -115,6 +145,7 @@ if __name__ == '__main__':
     digitalobject = optparse.OptionGroup(parser, 'Digital Object options')
     digitalobject.add_option('-t', '--title', dest='title', metavar='TITLE', help='information object title')
     digitalobject.add_option('-f', '--file', dest='file', metavar='FILE', help='file path')
+    digitalobject.add_option('-u', '--url', dest='url', metavar='URL', help='URL')
 
     parser.add_option_group(digitalobject)
 
@@ -128,8 +159,10 @@ if __name__ == '__main__':
       parser.error('You must provide login details (e-mail and password).')
     if opts.title is None:
       parser.error('You must provide a title.')
-    if opts.file is None:
-      parser.error('You must provide a file.')
+    if opts.file is None and opts.url is None:
+      parser.error('You must provide a file or URL.')
+    elif opts.file != None and opts.url != None:
+      parser.error('It is not possible to create a digital object from a file and URL simultaneously.')
 
     # Call main function
     upload({
@@ -137,6 +170,8 @@ if __name__ == '__main__':
       'password' : opts.password,
       'title' : opts.title,
       'file' : opts.file,
+      'url' : opts.url,
+      'debug' : opts.debug
       })
 
   except KeyboardInterrupt:
