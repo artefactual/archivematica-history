b2c59f135f8dea94fd273ad6063ef6957f2a45b5
Joseph Perry <berwin22@gmail.com>
2010-05-12 21:20:26 +0000

    Added copyright.
    Autoconverted from SVN (revision:310)

--- includes/archivematica/SIPxmlModifiers/CreateSipAndAddDublinCoreStructure.py
+++ includes/archivematica/SIPxmlModifiers/CreateSipAndAddDublinCoreStructure.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
+
+# This file is part of Archivematica.
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+# @package Archivematica
+# @subpackage Ingest
+# @author Joseph Perry <joseph@artefactual.com>
+# @version svn: $Id$
+
 #import xml.etree.cElementTree as etree
 import xml.etree.ElementTree as etree
 
