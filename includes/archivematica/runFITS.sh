900beea8d538a73056d3efdaf64337808a2b8982
Peter Van Garderen <vangarderen.peter@gmail.com>
2010-05-15 14:20:28 +0000

    change name of FITS logs
    Autoconverted from SVN (revision:357)

--- includes/archivematica/runFITS.sh
+++ includes/archivematica/runFITS.sh
@@ -2,7 +2,7 @@
 shortfile=`basename "$1"`
 
 if [ -e $1 ];then
-  cd /opt/externals/fits/ ; ./fits.sh -i $1 -o /home/demo/ingestLogs/$2/$shortfile.log
+  cd /opt/externals/fits/ ; ./fits.sh -i $1 -o /home/demo/ingestLogs/$2/FITS-$shortfile.xml
 else
   echo "file does not exist"
 fi
