04908dfb77a98a4dd5f8b457c44a5cf680de6eba
Joseph Perry <berwin22@gmail.com>
2010-08-03 22:08:19 +0000

    This can be used by the normalize.py script to determin if the system is out of space.
    Autoconverted from SVN (revision:439)

---
+++ includes/archivematica/freeSpaceChecker.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# This file is part of Archivematica.
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+# @package Archivematica
+# @subpackage Ingest
+# @author Joseph Perry <joseph@artefactual.com>
+# @version svn: $Id$
+
+import os
+import sys
+
+def checkSpace(drive = "/", space ="1024"):
+  hd=os.statvfs(drive)
+  freeSpace = (hd.f_bsize * hd.f_bavail)
+  # print "freespace: " + freeSpace.__str__() + "\nspace: " + long(space).__str__()
+  if freeSpace < long(space):
+    return True
+  else:
+    return False
+
+
+if __name__ == '__main__':
+  function =  sys.argv[1]
+
+  if function == "checkIfSpaceBelow" :
+    disk = sys.argv[2]
+    space = sys.argv[3]
+    tf = checkSpace(disk, space)
+    if tf == True:
+      print "true"
+    else:
+      print "false"
+
+  else:
+    print "Usage Error"
+
