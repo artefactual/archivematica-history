3cff8b703978a2c5e49084b04d98984da2a007b9
Joseph Perry <berwin22@gmail.com>
2010-08-20 01:03:01 +0000

    Working on the MCP protocol
    Autoconverted from SVN (revision:464)

--- includes/MCP/MCP/archivematicaMCP.py
+++ includes/MCP/MCP/archivematicaMCP.py
@@ -24,32 +24,59 @@ import os
 from archivematicaLoadConfig import loadConfig
 from modules.modules import modulesClass
 import pyinotify
-from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
+from pyinotify import WatchManager
+from pyinotify import Notifier
+from pyinotify import ThreadedNotifier
+from pyinotify import EventsCodes
+from pyinotify import ProcessEvent
+import uuid
+import threading
+import string
+from twisted.internet import reactor
+from twisted.internet import protocol
+from twisted.protocols.basic import LineReceiver
 
 archivmaticaVars = loadConfig("/home/joseph/sharedOnUServer/to build/archivematica/includes/archivematicaEtc/archivematicaConfig.conf")
 mask = pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO  #watched events
 configs = []
-#List of clients
-#List of jobs to queue
-#List of active jobs [UUID, folder to move when done.]
-
 jobsAwaitingApproval = []
 jobsQueue = []
+jobsBeingProcessed = []
+movingFolderLock = threading.Lock()
 
 def checkJobQueue():
     print "CHECING JOB QUEUE:"
     for job in jobsQueue:
-        print "  " + job.config.identifier + "\t" + job.folder.__str__() + "\t" + job.step
+        print "  " + job.UUID.__str__() + "\t" + job.config.identifier + "\t" + job.folder.__str__() + "\t" + job.step
+
+def assignTasks():
+    print "not implemented yet"
+    #compare tasks with nodes available.
+        #if tasks can process node, proceed to do so.
 
 class Job:
     def __init__(self, config, folder, step="exeCommand"):
-        #    self.UUID = UUID()
+        self.UUID = uuid.uuid4()
         self.config = config
         self.step = step
         self.folder = folder
+        
+    def queueJobStep(self):
+        print "run Job"
     
-
-    
+    def jobStepCompleted(self):
+        #if last step completed
+        if False:
+            #lock to ensure it doesn't start processing the next step, before the entire folder is moved.
+            movingFolderLock.acquire()
+            #move folder to next location, depending on fail status
+            
+            movingFolderLock.release()
+            #remove this job from the jobsQueue
+            jobsBeingProcessed.remove(self)
+        else:
+            print "Queue Next step"
+            
 
 class watchFolder(ProcessEvent):
     config = None
@@ -58,19 +85,20 @@ class watchFolder(ProcessEvent):
     def process_IN_CREATE(self, event):
         print "Warning: %s was created. Was something copied into this folder?" %  os.path.join(event.path, event.name)
         
-    def process_IN_MOVED_TO(self, event):      
+    def process_IN_MOVED_TO(self, event):  
+        #ensure no folders are in the process of moving. (so none will be in the middle of moving INTO this folder)
+        movingFolderLock.acquire()
+        movingFolderLock.release()    
+        
         job = Job(self.config, os.path.join(event.path, event.name))
         if self.config.requiresUserApproval:
             #print "need to get user approval"
             jobsAwaitingApproval.append(job)
+            #dashboard Alert - new job needing approval.
+            
         else:
             jobsQueue.append(job)
             checkJobQueue()
-
-            
-          
-
-
 
 def loadConfigs():
     configFiles = []
@@ -93,37 +121,32 @@ def loadFolderWatchLlist(configs):
         wdd = wm.add_watch(config.watchFolder, mask, rec=False)
         notifier.start()
 
-
-"""
-def MCPclient()
-        request info:
-            give info
-        approveJob:
-            add job to que
-            
-def clientConnect(client UUID, client nice name, connect back info)
-    getIP/connection info
-    connect back to client.(to issues commands to client)
-    return configDic
-    #connection terminates
+class archivematicaMCPServerProtocol(LineReceiver):
+    """This is just about the simplest possible protocol"""
     
-def processQue
-    While(1):
-        take que task
-        send task to available client, with taskCompleted() ret, and JobUUID., relative path the files can be found (relative to shared folder.
-        
-def taskCompleted(jobUUID):
-    make note in the logs
-    if the 
+    def lineReceived(self, line):
+        "As soon as any data is received, write it back."
+        print "read: " + line.__str__()
+        self.write(line)
 
-"""
+    def connectionMade(self):
+        self.write("hello, client!")
+    
+    def write(self,line):
+        self.transport.write(line)
+        print "wrote: " + line.__str__()
+
+def archivematicaMCPServerListen():
+    factory = protocol.ServerFactory()
+    factory.protocol = archivematicaMCPServerProtocol
+    reactor.listenTCP(string.atoi(archivmaticaVars["MCPArchivematicaServerPort"]),factory)
+    reactor.run()
+
 
 if __name__ == '__main__':
     configs = loadConfigs()
     folderWatchList = loadFolderWatchLlist(configs)
+    archivematicaMCPServerListen()
 #    Start listening for client connections (new thread) 
 #    Start listening for MCPclient Connections.
-    
-#    JOB UUID - operating on the entire SIP folder
-#    Task - A job can contain many tasks- a task is per file
-    
+
