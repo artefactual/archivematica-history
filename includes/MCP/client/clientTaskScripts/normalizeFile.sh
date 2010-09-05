0e544dfa9fd0bb5d15981198f92d0b641d2c9603
Joseph Perry <berwin22@gmail.com>
2010-09-05 20:13:31 +0000

    preparing to start recreating the archivematica 0.6 workflow.
    Autoconverted from SVN (revision:482)

--- includes/MCP/client/clientTaskScripts/normalizeFile.sh
+++
@@ -1,4 +0,0 @@
-Should be able to instead directly call normalizy.py
-Unfortunatly, there is no function/method to call to issue it at this time.
-
-Archivematica will force the use of it's configurable normalyze.py script.
--- includes/MCP/client/clientTaskScripts/setFilePermissions.sh
+++
@@ -1,4 +0,0 @@
-find /tmp/$UUID/ -type f -print| while read NEWDOCS
-    do
-        chmod 700 $NEWDOCS
-    done
