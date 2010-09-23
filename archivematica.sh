09a0e3972d90fc2adeec984761f2d3f8f575313a
Austin Trask <austin.trask@gmail.com>
2010-09-23 15:25:35 +0000

    cleanup of archivematica.sh
    Autoconverted from SVN (revision:514)

--- archivematica.sh
+++ archivematica.sh
@@ -23,15 +23,13 @@
 
 
 #Prepare Ubuntu
-chroot $1 chmod -R 777 /etc/apt/sources.list
-chroot $1 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-chroot $1 sed -i -e "s/# deb/deb/g" /etc/apt/sources.list
-chroot $1 sed -i -e "s/localhost:9999/archive.ubuntu.com/g" /etc/apt/sources.list
-chroot $1 sed -i -e "s/127.0.0.1:9999/archive.ubuntu.com/g" /etc/apt/sources.list
+chmod -R 777 $/etc/apt/sources.list
+cp /etc/apt/sources.list $1/etc/apt/sources.list.bak
+sed -i -e "s/# deb/deb/g" $1/etc/apt/sources.list
+sed -i -e "s/localhost:9999/archive.ubuntu.com/g" $1/etc/apt/sources.list
+sed -i -e "s/127.0.0.1:9999/archive.ubuntu.com/g" $1/etc/apt/sources.list
 echo "deb http://archive.ubuntu.com/ubuntu/ karmic-proposed main restricted universe multiverse" >> $1/etc/apt/sources.list
 chroot $1 aptitude update
-#chroot $1 aptitude -q -Ry build-dep -d openjdk-6-jre jacksum openoffice.org-java-common
-#chroot $1 aptitude -t jaunty-proposed install openjdk-6-jre jacksum openoffice.org-java-common
 chroot $1 rm -rf /home/demo/Documents
 chroot $1 rm -rf /home/demo/Music
 chroot $1 rm -rf /home/demo/Pictures
@@ -67,74 +65,79 @@ chroot $1 update-python-modules
 
 #Install externals/archivematica
 chroot $1 mkdir -p /home/demo/Desktop
-chroot $1 mkdir /var/7-storeAIP
-chroot $1 mkdir /var/1-receiveSIP
-chroot $1 ln -s /var/7-storeAIP/ /home/demo
-chroot $1 ln -s /var/1-receiveSIP/ /home/demo
-svn export includes/sampledata $1/home/demo/testFiles
-svn export includes/sampledata/ImagesSIP $1/var/1-receiveSIP/ImagesSIP
-svn export includes/sampledata/MultimediaSIP $1/var/1-receiveSIP/MultimediaSIP
-svn export includes/sampledata/OfficeDocsSIP $1/var/1-receiveSIP/OfficeDocsSIP
-svn export includes/externals $1/opt/externals
 svn export includes/.mozilla $1/home/demo/.mozilla
 svn export includes/xenaconfig $1/home/demo/.java
 svn export includes/Docs $1/home/demo/Docs
 chroot $1 ln -s /home/demo/Docs /home/demo/Desktop
-cp -rf includes/cron.tab $1/etc/cron.tab
-chroot $1 crontab -u demo /etc/cron.tab
-cp -rf includes/incron.allow $1/etc/incron.allow
-cp -rf includes/incron.tab $1/etc/incron.tab
-chroot $1 incrontab /etc/incron.tab
+
+#cron and incron no longer needed with MCP
+#cp -rf includes/cron.tab $1/etc/cron.tab
+#chroot $1 crontab -u demo /etc/cron.tab
+#cp -rf includes/incron.allow $1/etc/incron.allow
+#cp -rf includes/incron.tab $1/etc/incron.tab
+#chroot $1 incrontab /etc/incron.tab
+
+#setup unonconv service
 cp -rf includes/unoconv-listen $1/etc/init.d/unoconv-listen
 chroot $1 chmod +x /etc/init.d/unoconv-listen
 chroot $1 /usr/sbin/update-rc.d unoconv-listen defaults
 chroot $1 chmod -R 770 /home/demo/.mozilla
 chroot $1 mkdir -p /home/demo/.gnome2/nautilus-scripts
-chroot $1 mkdir /home/demo/2-reviewSIP
-chroot $1 mkdir /home/demo/3-quarantineSIP
-chroot $1 mkdir /home/demo/4-appraiseSIP
-chroot $1 mkdir /home/demo/5-prepareAIP
-chroot $1 mkdir /home/demo/6-reviewAIP	
-chroot $1 mkdir /home/demo/8-reviewDIP
-chroot $1 mkdir /home/demo/9-uploadDIP
-chroot $1 mkdir /home/demo/ingestLogs
-chroot $1 mkdir /home/demo/SIPerrors
-chroot $1 mkdir /home/demo/SIPerrors/normalizationErrors
-chroot $1 mkdir /home/demo/SIPerrors/possibleVirii
-chroot $1 mkdir /home/demo/SIPerrors/rejectedSIPs
+
+#Should we link these to the new shared folder structure?
+#chroot $1 mkdir /home/demo/2-reviewSIP
+#chroot $1 mkdir /home/demo/3-quarantineSIP
+#chroot $1 mkdir /home/demo/4-appraiseSIP
+#chroot $1 mkdir /home/demo/5-prepareAIP
+#chroot $1 mkdir /home/demo/6-reviewAIP	
+#chroot $1 mkdir /home/demo/8-reviewDIP
+#chroot $1 mkdir /home/demo/9-uploadDIP
+#chroot $1 mkdir /home/demo/ingestLogs
+#chroot $1 mkdir /home/demo/SIPerrors
+#chroot $1 mkdir /home/demo/SIPerrors/normalizationErrors
+#chroot $1 mkdir /home/demo/SIPerrors/possibleVirii
+#chroot $1 mkdir /home/demo/SIPerrors/rejectedSIPs
+#chroot $1 mkdir /var/7-storeAIP
+#chroot $1 mkdir /var/1-receiveSIP
+#chroot $1 ln -s /var/7-storeAIP/ /home/demo
+#chroot $1 ln -s /var/1-receiveSIP/ /home/demo
+
+#where should these exports go?
+#svn export includes/sampledata $1/home/demo/testFiles
+#svn export includes/sampledata/ImagesSIP $1/var/1-receiveSIP/ImagesSIP
+#svn export includes/sampledata/MultimediaSIP $1/var/1-receiveSIP/MultimediaSIP
+#svn export includes/sampledata/OfficeDocsSIP $1/var/1-receiveSIP/OfficeDocsSIP
+
+#XFCE configuration
 chroot $1 mkdir /home/demo/.config
 chroot $1 mkdir /home/demo/.config/Thunar
 chroot $1 mkdir /home/demo/.config/autostart
 chroot $1 mkdir -p /home/demo/.config/xfce4/desktop
 chroot $1 mkdir -p /home/demo/.config/xfce4/panel
 
-#Gnome Integration/Customization
-#cp -p includes/makeMD5 $1/home/demo/.gnome2/nautilus-scripts
-#cp -p includes/checkMD5 $1/home/demo/.gnome2/nautilus-scripts
-#cp -p includes/Bagit $1/home/demo/.gnome2/nautilus-scripts
-#cp -p includes/bagcheck $1/home/demo/.gnome2/nautilus-scripts
+#add archivematica/dashboard icons
 cp includes/dashboard-desktop-icon.png $1/usr/share/icons
 cp includes/dcb-desktop-icon.png $1/usr/share/icons
 cp includes/ica-atom-desktop-icon.png $1/usr/share/icons
 cp includes/archivematica-xubuntu-steel.png $1/usr/share/xfce4/backdrops/xubuntu-karmic.png
-
 cp includes/ica-atom.desktop $1/home/demo/Desktop
 cp includes/droid.desktop $1/home/demo/Desktop
 cp includes/jhove.desktop $1/home/demo/Desktop
-#cp includes/NLNZ-metadata-extractor.desktop $1/home/demo/Desktop
-#cp includes/xena.desktop $1/home/demo/Desktop
-#cp includes/view.desktop $1/home/demo/Desktop
 cp includes/dcb.desktop $1/home/demo/Desktop
 #cp includes/dashboard.desktop $1/home/demo/Desktop
 cp includes/WinFF.desktop $1/home/demo/Desktop
 cp includes/Terminal.desktop $1/home/demo/Desktop
-cp includes/rundroid.sh $1/usr/bin
+
+#add launcher scripts
 cp includes/runica-atom.sh $1/usr/bin
 cp includes/rundcb.sh $1/usr/bin
 cp includes/rundashboard.sh $1/usr/bin
 cp includes/runjhove.sh $1/usr/bin
+
+#these are now created by their debian packages
 #cp includes/runxena.sh $1/usr/bin
 #cp includes/runview.sh $1/usr/bin
+#cp includes/rundroid.sh $1/usr/bin
 
 #xfce4 configuration
 cp includes/panel/* $1/home/demo/.config/xfce4/panel
@@ -173,11 +176,12 @@ svn export ica-atom-svn $1/var/www/ica-atom
 svn export dcb-svn $1/var/www/dcb
 svn export qubit-svn $1/var/www/qubit
 cp includes/dashboard $1/var/www/dashboard
+chroot $1 sh -c 'echo EnableSendfile Off >> /etc/apache2/apache2.conf'
+chroot $1 a2enmod rewrite
 
-apache2ctl restart
+#fix ownership of web apps
 chroot $1 chown -R www-data:www-data /var/www/ica-atom
 chroot $1 chown -R www-data:www-data /var/www/dcb
 chroot $1 chown -R www-data:www-data /var/www/qubit
-chroot $1 sh -c 'echo EnableSendfile Off >> /etc/apache2/apache2.conf'
-chroot $1 a2enmod rewrite
+
 
