19e7a357ae3b79f565a5223ef2951049371ffa98
Joseph Perry <berwin22@gmail.com>
2010-10-05 03:25:22 +0000

    This allows user to select which VM to build.
    Autoconverted from SVN (revision:538)

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
+#!/bin/sh
 #This script  will be modified to allow user to select the type of vm to build.
-sudo vmbuilder vmserver ubuntu -c archivematica.cfg -d "archivematicaBuild" --rootsize 8000
-mv ./archivematicaBuild "./archivematicaBuild-`date`"
+cat << !
+Build Archivematica VM
+1. KVM
+2. vmware
+3. xen
+q. Quit
+!
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+1) vmType="kvm" ;;
+2) vmType="vmserver" ;;
+3) vmType="xen" ;;
+q) exit ;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+
+sudo vmbuilder "$vmType" ubuntu -c archivematica.cfg -d "archivematicaBuild-`date +"%Y.%m.%d %k.%M"`" --rootsize 8000
+
