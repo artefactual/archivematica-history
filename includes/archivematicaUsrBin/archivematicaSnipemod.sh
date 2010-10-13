8f0f2405c9a17c5b831ffdefb9b038dfc720f87a
Austin Trask <austin.trask@gmail.com>
2010-10-13 01:56:52 +0000

    cleanup of archivematicaUsrBin
    Autoconverted from SVN (revision:561)

--- includes/archivematicaUsrBin/archivematicaSnipemod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-touch -a /home/demo/3-quarantineSIP/"$*"
-chmod -R 000 /home/demo/3-quarantineSIP/"$*"
--- includes/archivematicaUsrBin/normalize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-shortfile=`basename "$1"`
-
-if [ -e $1 ];then
-  cd /opt/externals/xena/ ; java -jar xena.jar -f $1 -o $2 -p plugins/ >> /home/demo/ingestLogs/$3/normalization.log
-else
-  echo "file does not exist"
-fi
