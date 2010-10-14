b7e77bad7023b062a220df29b7ef7952bcc62f4f
Austin Trask <austin.trask@gmail.com>
2010-10-14 23:43:17 +0000

    removed xscreensaver package in the postbuildscript
    Autoconverted from SVN (revision:568)

--- postBuildScripts/postBuildRun.sh
+++ postBuildScripts/postBuildRun.sh
@@ -9,7 +9,7 @@ sudo chmod 770 -R  ~/sharedDirectory/
 sudo chown -R archivematica:archivematica ~/sharedDirectory/
 sudo chmod -R g+s ~/sharedFolders/
 echo "Disabling Screen Saver (Better for VM's)"
-gconftool-2 -s /apps/gnome-screensaver/idle_activation_enabled --type=bool false
+sudo aptitude remove xscreensaver
 sudo gpasswd -a demo archivematica
 echo "PLEASE REBOOT TO ENABLE NEW GROUP SETTINGS"
 sleep 3
