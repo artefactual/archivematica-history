d0a0481ad84cd5c3d931f0f225c84380d9564e63
Austin Trask <austin.trask@gmail.com>
2010-10-15 16:57:45 +0000

    reverted to sharedDirectory in postBuildRun.sh
    Autoconverted from SVN (revision:570)

--- postBuildScripts/postBuildRun.sh
+++ postBuildScripts/postBuildRun.sh
@@ -5,9 +5,9 @@ sudo mysqladmin create dcb
 sudo mysqladmin create qubit
 sudo mysqladmin create dashboard
 sudo chmod 444 -R ~/.config/xfce4/panel
-sudo chmod 770 -R  ~/sharedDirectories/
-sudo chown -R archivematica:archivematica ~/sharedDirectories/
-sudo chmod -R g+s ~/sharedDirectories/
+sudo chmod 770 -R  ~/sharedDirectory/
+sudo chown -R archivematica:archivematica ~/sharedDirectory/
+sudo chmod -R g+s ~/sharedDirectory/
 echo "Disabling Screen Saver (Better for VM's)"
 sudo aptitude remove xscreensaver
 sudo gpasswd -a demo archivematica
