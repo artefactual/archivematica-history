665336f32e746fdf3ce99f524bee7a5d0a1b48c0
Austin Trask <austin.trask@gmail.com>
2010-11-21 10:54:12 +0000

    added MCPSerer setup scripts to /usr/share/archivematica/
    Autoconverted from SVN (revision:676)

--- buildVM/includes/postBuildScripts/postBuildRun.sh
+++ buildVM/includes/postBuildScripts/postBuildRun.sh
@@ -1,14 +1,14 @@
 echo "The default password is demo"
 cd postBuildRunAssistScripts
-./preMCPLogging.sh
+#./preMCPLogging.sh
 sudo mysqladmin create ica-atom
 sudo mysqladmin create dcb
 sudo mysqladmin create qubit
 sudo mysqladmin create dashboard
 sudo chmod 444 -R ~/.config/xfce4/panel
-sudo chmod 770 -R  ~/sharedDirectory/
-sudo chown -R archivematica:archivematica ~/sharedDirectory/
-sudo chmod -R g+s ~/sharedDirectory/
+sudo chmod 770 -R  /var/archivematica/sharedDirectory/
+sudo chown -R archivematica:archivematica /var/archivematica/sharedDirectory/
+sudo chmod -R g+s /var/archivematica/sharedDirectory/
 echo "Disabling Screen Saver (Better for VM's)"
 sudo aptitude remove xscreensaver
 sudo gpasswd -a demo archivematica
