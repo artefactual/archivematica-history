4d1d5a464fec65e78dd02d7d2d16b0ff528981d5
Jesús García Crespo <jesus@sevein.com>
2010-11-28 18:36:52 +0000

    Add MCP client library.
    Autoconverted from SVN (revision:755)

---
+++ src/dashboard/src/contrib/mcpclient/client.py
@@ -0,0 +1,14 @@
+import os
+from xmlrpclib import ServerProxy, Error
+
+class MCPClient:
+
+  def __init__(self, host = 'localhost', port = '8000'):
+    self.url = 'http://%s:%d' % (host, port)
+    self.server = ServerProxy(self.url)
+
+  def approve_job(self):
+    pass
+
+  def get_jobs_awaiting_approval(self):
+    return self.server.getJobsAwaitingApproval()
