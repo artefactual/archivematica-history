7e1c514cb5b767e9ccd0e308f4b6ce6c9a8494c2
Jesús García Crespo <jesus@sevein.com>
2010-11-29 23:55:24 +0000

    Implement approve_jobs method.
    Autoconverted from SVN (revision:791)

--- src/dashboard/src/contrib/mcp/client.py
+++ src/dashboard/src/contrib/mcp/client.py
@@ -6,8 +6,8 @@ class MCPClient:
     self.url = 'http://%s:%d' % (host, port)
     self.server = ServerProxy(self.url)
 
-  def approve_job(self):
-    pass
+  def approve_job(self, uuid):
+    return self.server.approveJob(uuid)
 
   def get_jobs_awaiting_approval(self):
     return self.server.getJobsAwaitingApproval()
