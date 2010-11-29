47472128ec2771fb9b0bbf517ed69b28231ed3d2
Austin Trask <austin.trask@gmail.com>
2010-11-29 00:41:53 +0000

    runica-atom.sh modified path
    Autoconverted from SVN (revision:775)

--- buildVM/includes/desktopShortcuts/runica-atom.sh
+++ buildVM/includes/desktopShortcuts/runica-atom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ~/.mozilla -name extensions.cache -exec rm -f {} \; && firefox http://localhost
+find ~/.mozilla -name extensions.cache -exec rm -f {} \; && firefox http://localhost/ica-atom
