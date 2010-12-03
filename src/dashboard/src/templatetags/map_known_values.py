4d25d7bff8e6ccb7192711d700372af9b45c3138
Jesús García Crespo <jesus@sevein.com>
2010-12-03 13:19:45 +0000

    Add templatetags directory and map_known_values filter.
    Autoconverted from SVN (revision:823)

---
+++ src/dashboard/src/templatetags/map_known_values.py
@@ -0,0 +1,42 @@
+from django import template
+register = template.Library()
+
+@register.filter('map_known_values')
+def map_known_values(value):
+
+  map = {
+    'acquireSIP': 'Acquire SIP',
+    'addDCToMETS': 'Add DC to METS',
+    'appraiseSIP': 'Appraise SIP',
+    'assignSIPUUID': 'Asign SIP UUID',
+    'assignUUID': 'Assign UUID',
+    'bagit': 'Bagit',
+    'cleanupAIPPostBagit': 'Cleanup AIP post bagit',
+    'compileMETS': 'Compile METS',
+    'copyMETSToDIP': 'Copy METS to DIP',
+    'createAIPChecksum': 'Create AIP checksum',
+    'createDIPDirectory': 'Create DIP directory',
+    'createOrMoveDC': 'Create or move DC',
+    'createSIPBackup': 'Create SIP backup',
+    'detoxFileNames': 'Detox filenames',
+    'extractPackage': 'Extract package',
+    'FITS': 'FITS',
+    'normalize': 'Normalize',
+    'quarantine': 'Quarantine',
+    'reviewSIP': 'Review SIP',
+    'scanForRemovedFilesPostAppraiseSIPForPreservation': 'Scan for removed files post appraise SIP for preservation',
+    'scanForRemovedFilesPostAppraiseSIPForSubmission': 'Scan for removed files post appraise SIP for submission',
+    'scanWithClamAV': 'Scan with ClamAV',
+    'seperateDIP': 'Seperate DIP',
+    'storeAIP': 'Store AIP',
+    'unquarantine': 'Unquarantine',
+    'uploadDIP': 'Upload DIP',
+    'verifyChecksum': 'Verify checksum',
+    'verifyMetadataDirectoryChecksums': 'Verify metadata directory checksums',
+    'verifySIPCompliance': 'Verify SIP compliance',
+  }
+
+  if value in map:
+    return map[value]
+  else:
+    return value
