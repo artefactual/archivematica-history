8e27f90f2e1d7301b3951c1274e47ea9661146cc
Joseph Perry <berwin22@gmail.com>
2010-12-22 19:32:34 +0000

    Changed permissions on demo mysql user to be strict.
    Autoconverted from SVN (revision:940)

--- localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
+++ localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
@@ -6,7 +6,7 @@ sudo mysqladmin create "$databaseName" $dpPassword
 #sudo mysql $databaseName
 sudo mysql $dpPassword --execute="source ./preMCPLogging.sql" "$databaseName"
 sudo mysql $dpPassword --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
-sudo mysql $dpPassword --execute="GRANT ALL PRIVILEGES ON *.* TO 'demo'@'localhost'"
+sudo mysql $dpPassword --execute="GRANT SELECT, UPDATE, INSERT ON ${databaseName}.* TO '${username}'@'localhost'"
 
 
 #to delete the database and all of it's contents
