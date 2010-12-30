d55674d1ec7d0fce9fc9329965cfdd485bdd658a
Austin Trask <austin.trask@gmail.com>
2010-12-30 21:51:11 +0000

    switched the dev setup to use tables supplied by source package
    Autoconverted from SVN (revision:961)

--- localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
+++ localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
@@ -4,7 +4,7 @@ password="demo"
 dpPassword="$1"
 sudo mysqladmin create "$databaseName" $dpPassword
 #sudo mysql $databaseName
-sudo mysql $dpPassword --execute="source ./preMCPLogging.sql" "$databaseName"
+sudo mysql $dpPassword --execute="source ../../../src/MCPServer/share/mysql" "$databaseName"
 sudo mysql $dpPassword --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
 sudo mysql $dpPassword --execute="GRANT SELECT, UPDATE, INSERT ON ${databaseName}.* TO '${username}'@'localhost'"
 
