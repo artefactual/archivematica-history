b295984aea01a96621bdcc69728f590da98a2110
Austin Trask <austin.trask@gmail.com>
2011-01-03 22:39:01 +0000

    added dosfstools to local dev setup
    Autoconverted from SVN (revision:965)

--- localDevSetup/installLocalDevRequirements.sh
+++ localDevSetup/installLocalDevRequirements.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libapache2-mod-wsgi python-django python-django-doc python-twisted python-pyinotify ffmpeg2theora libavcodec-unstripped-52 flashplugin-installer unoconv openjdk-6-jre openoffice.org openoffice.org-java-common libnotify-bin uuid httrack sendemail curl clamav incron nfs-common flac md5deep ffmpeg winff firebug imagemagick libapache2-mod-php5 mysql-server php5-cli php5-mysql php5-xsl subversion-tools par2 unrar p7zip-full python-execnet digikam kipi-plugins python-mysqldb nfs-kernel-server python-lxml inkscape
+sudo apt-get install libapache2-mod-wsgi python-django python-django-doc python-twisted python-pyinotify ffmpeg2theora libavcodec-unstripped-52 flashplugin-installer unoconv openjdk-6-jre openoffice.org openoffice.org-java-common libnotify-bin uuid httrack sendemail curl clamav incron nfs-common flac md5deep ffmpeg winff firebug imagemagick libapache2-mod-php5 mysql-server php5-cli php5-mysql php5-xsl subversion-tools par2 unrar p7zip-full python-execnet digikam kipi-plugins python-mysqldb nfs-kernel-server python-lxml inkscape dosfstools
 ##LUCID
 #sudo add-apt-repository ppa:austin-arcintel/archivematica
 ##OTHERS
