b668948d7577725a2e56d2bae66544ad79f74ced
Jesús García Crespo <jesus@sevein.com>
2011-01-23 22:14:55 +0000

    Migration to backbone.js.
    Autoconverted from SVN (revision:1023)

--- src/dashboard/src/main/views.py
+++ src/dashboard/src/main/views.py
@@ -48,26 +48,88 @@ def get_all(request):
   def encoder(obj):
     items = []
     for item in obj:
-      jobs = Job.objects.filter(sipuuid = item['sipuuid'])
+      jobs = get_jobs_by_sipuuid(item['sipuuid'])
       directory = jobs[0].directory
       item['directory'] = re.search(r'^.*/(?P<directory>.*)-[\w]{8}(-[\w]{4}){3}-[\w]{12}$', directory).group('directory')
       item['timestamp'] = item['timestamp'].strftime('%x %X')
+      item['status'] = 0
       item['uuid'] = item['sipuuid']
       del item['sipuuid']
       try: jobsAwaitingApprovalXml
       except NameError: pass
       else:
+        item['jobs'] = []
         for job in jobs:
+          newJob = {}
+          newJob['uuid'] = job.jobuuid
+          newJob['microservice'] = map_known_values(job.jobtype)
+          newJob['currentstep'] = map_known_values(job.currentstep)
+          item['jobs'].append(newJob)
           for uuid in jobsAwaitingApprovalXml.findall('Job/UUID'):
             if uuid.text == job.jobuuid:
               item['status'] = 1
-              item['job'] = job.jobuuid
               break
-          if 'status' in item:
-            break
-      if 'status' not in item:
-        item['status'] = 0
       items.append(item)
     return items
   response = simplejson.JSONEncoder(default=encoder).encode(objects)
   return HttpResponse(response, mimetype='application/json')
+
+def map_known_values(value):
+  map = {
+    # currentStep
+    'completedSuccessfully': 'Completed successfully',
+    'completedUnsuccessfully': 'Failed',
+    'exeCommand': 'Executing',
+    'requiresAprroval': 'Requires approval',
+    'requiresApproval': 'Requires approval',
+    # jobType
+    'acquireSIP': 'Acquire SIP',
+    'addDCToMETS': 'Add DC to METS',
+    'appraiseSIP': 'Appraise SIP',
+    'assignSIPUUID': 'Asign SIP UUID',
+    'assignUUID': 'Assign file UUIDs and checksums',
+    'bagit': 'Bagit',
+    'cleanupAIPPostBagit': 'Cleanup AIP post bagit',
+    'compileMETS': 'Compile METS',
+    'copyMETSToDIP': 'Copy METS to DIP',
+    'createAIPChecksum': 'Create AIP checksum',
+    'createDIPDirectory': 'Create DIP directory',
+    'createOrMoveDC': 'Create or move DC',
+    'createSIPBackup': 'Create SIP backup',
+    'detoxFileNames': 'Detox filenames',
+    'extractPackage': 'Extract package',
+    'FITS': 'FITS',
+    'normalize': 'Normalize',
+    'quarantine': 'Place in quarantine',
+    'reviewSIP': 'Review SIP',
+    'scanForRemovedFilesPostAppraiseSIPForPreservation': 'Scan for removed files post appraise SIP for preservation',
+    'scanForRemovedFilesPostAppraiseSIPForSubmission': 'Scan for removed files post appraise SIP for submission',
+    'scanWithClamAV': 'Scan with ClamAV',
+    'seperateDIP': 'Seperate DIP',
+    'storeAIP': 'Store AIP',
+    'unquarantine': 'Remove from Quarantine',
+    'uploadDIP': 'Upload DIP',
+    'verifyChecksum': 'Verify checksum',
+    'verifyMetadataDirectoryChecksums': 'Verify metadata directory checksums',
+    'verifySIPCompliance': 'Verify SIP compliance',
+  }
+  if value in map:
+    return map[value]
+  else:
+    return value
+
+def get_jobs_by_sipuuid(uuid):
+  jobs = Job.objects.all().filter(sipuuid = uuid).order_by('-createdtime')
+  priorities = {
+    'completedUnsuccessfully': 0,
+    'requiresAprroval': 1,
+    'requiresApproval': 1,
+    'exeCommand': 2,
+    'verificationCommand': 3,
+    'completedSuccessfully': 4,
+    'cleanupSuccessfulCommand': 5,
+  }
+  def get_priority(job):
+    try: return priorities[job.currentstep]
+    except Exception: return 0
+  return sorted(jobs, key = get_priority) # key = lambda job: priorities[job.currentstep]
--- src/dashboard/src/media/js/sips.js
+++ src/dashboard/src/media/js/sips.js
@@ -1,280 +1,169 @@
-// Dashboard namespace
-var Dashboard = {};
-  // Dashboard.IntervalManager
-  // Dashboard.SipManager
- 
-Dashboard.pollingInterval = 5; // Seconds
-
-Dashboard.SipManager = function()
+$(function()
   {
-    this.$container = $('#content');
-    this.sips = [];
 
-    this.isActive = false;
-    this.interval = Dashboard.pollingInterval * 1000;
+    window.Sip = Backbone.Model.extend({
 
-    this.statusWidget = {
-      widget: $('<div id="status"><div><div><span>&nbsp;</span></div></div></div>').hide().appendTo(document.body),
-      show: function(message, error)
+      initialize: function()
         {
-          this.text(message);
+          this.jobs = new JobCollection(this.get('jobs'));
+        }
 
-          if (true === error)
+    });
+
+    window.SipCollection = Backbone.Collection.extend({
+
+      model: Sip,
+      
+      url: '/sips/all/'
+      
+    });
+
+    window.SipView = Backbone.View.extend({
+
+      className: 'sip',
+
+      template: _.template($('#sip-template').html()),
+      
+      events: {
+        'click .sip-row > .sip-detail-jobs > a': 'toggleJobs',
+      },
+
+      initialize: function()
+        {
+          _.bindAll(this, 'render');
+          this.model.view = this;
+        },
+      
+      render: function()
+        {
+          $(this.el).html(this.template(this.model.toJSON())).attr('uuid', this.model.get('uuid'));
+
+          if (this.model.get('status'))
           {
-            this.widget.addClass('status-error');
+            $(this.el).addClass('sip-highlight');
+          }
+
+          var self = this;
+
+          this.$('.sip-detail-icon').html(function()
+            {
+              var $img = $('<img />');
+              switch (self.model.get('status'))
+              {
+                case 0:
+                  $img.attr('src', '/media/images/accept.png');
+                  break;
+
+                case 1:
+                  $img.attr('src', '/media/images/bell.png');
+                  break;
+              }
+
+              return $img;
+            });
+
+          this.model.jobs.each(function(job)
+            {
+              var view = new JobView({model: job});
+              self.$('.sip-detail-job-container').append(view.render().el);
+            });
+
+          return this;
+        },
+
+      toggleJobs: function()
+        {
+          var $jobContainer = this.$('.sip-detail-job-container');
+
+          if ($jobContainer.is(':visible'))
+          {
+            $jobContainer.slideUp('fast');
+            $(this.el).removeClass('sip-selected');
+            this.$('.sip-detail-jobs > a').text('Show jobs');
           }
           else
           {
-            this.widget.removeClass('status-error');
+            $jobContainer.slideDown('fast');
+            $(this.el).addClass('sip-selected');
+            this.$('.sip-detail-jobs > a').text('Hide jobs');
           }
-
-          this.widget.show();
-        },
-      hide: function()
-        {
-          this.widget.fadeOut(500);
-        },
-      text: function(message)
-        {
-          this.widget.find('span').html(message);
         }
-    };
-
-    var self = this;
-    this.$container
-      .delegate('.sip', 'click', function(event)
-        {
-          var sip = self.get(this.getAttribute('uuid'));
-        })
-      .delegate('.sip', 'hover', function(event)
-        {
-          if ('mouseenter' == event.type)
-          {
-            $(this).addClass('sip-hover');
-          }
-          else if ('mouseleave' == event.type)
-          {
-            $(this).removeClass('sip-hover');
-          }
-        });
-  };
-
-Dashboard.SipManager.prototype.get = function(uuid)
-  {
-    for (var i in this.sips)
-    {
-      if (uuid == this.sips[i].uuid)
-      {
-        return this.sips[i];
-      }
-    }
-  };
-
-Dashboard.SipManager.prototype.load = function()
-  { 
-    $.ajax({
-      beforeSend: function()
-        {
-          this.statusWidget.show('Loading...');
-        },
-      context: this,
-      dataType: 'json',
-      error: function()
-        {
-          var self = this;
-          var counter = 0;
-          var timerID = setInterval(function()
-            {
-              var icounter = 5 - counter;
-
-              if (icounter == 0)
-              {
-                clearInterval(timerID);
-                self.start();
-
-                return true;
-              }
-
-              self.statusWidget.show('Error connecting to server... trying again in ' + icounter + 's.', true);
-              counter++;
-            }, 1000);
-        },
-      success: function(data)
-        {
-          for (var i in data)
-          {
-            // Add Sips
-            this.add(data[i]);
-          }
-
-          this.statusWidget.hide();
-          this.step();
-        },
-      type: 'GET',
-      url: '/sips/all/',
     });
-  };
 
-Dashboard.SipManager.prototype.add = function(sip)
-  {
-    var existingSip = this.find(sip.uuid);
-
-    if (false === existingSip)
-    {
-      var newSip = new Dashboard.Sip(sip);
-      this.sips.push(newSip);
-      newSip.render();
-    }
-    else
-    {
-      existingSip.replace(sip);
-    }
-  };
-
-Dashboard.SipManager.prototype.find = function(uuid)
-  {
-    for (var i in this.sips)
-    {
-      if (this.sips[i].uuid == uuid)
-      {
-        return this.sips[i];
-      }
-    }
-
-    return false;
-  };
-
-Dashboard.SipManager.prototype.render = function()
-  {
-    var $sipsContainer = $('<div id="sips-container" />').append(
-      '<div id="sips-header">' +
-      '<div id="sips-header-icon">&nbsp;</div>' +
-      '<div id="sips-header-directory">Directory</div>' +
-      '<div id="sips-header-uuid">UUID</div>' +
-      '<div id="sips-header-timestamp">Timestamp</div>' +
-      '<div id="sips-header-jobs">&nbsp;</div>' +
-      '</div>');
-
-    $sipsContainer.appendTo(this.$container);
-  };
-
-Dashboard.SipManager.prototype.start = function()
-  {
-    this.setActive(true);
-  };
-
-Dashboard.SipManager.prototype.stop = function()
-  {
-    this.setActive(false);
-  };
-
-Dashboard.SipManager.prototype.toggle = function()
-  {
-    if (this.isActive === false)
-    {
-      this.start();
-    }
-    else
-    {
-      this.stop();
-    }
-  };
-
-Dashboard.SipManager.prototype.setActive = function(active)
-  {
-    if (active === true)
-    {
-      this.isActive = true;
-      this.render();
-      this.load();
-    }
-    else if (active === false)
-    {
-      this.isActive = false;
-    }
-  };
-
-Dashboard.SipManager.prototype.step = function()
-  {
-    if (this.isActive === false)
-    {
-      return false;
-    }
-
-    var self = this;
-    setTimeout(function()
-      {
-        self.load();
-      }, this.interval);
-  };
-
-Dashboard.Sip = function()
-  {
-    if (1 != arguments.length)
-    {
-      return false;
-    }
-
-    this.$container = $('#sips-container');
-
-    this.load(arguments[0]);
-    this.build();
-  };
-
-Dashboard.Sip.prototype.load = function(sip)
-{
-  this.directory = sip.directory;
-  this.uuid = sip.uuid;
-  this.timestamp = sip.timestamp;
-  this.status = sip.status;
-}
-
-Dashboard.Sip.prototype.getIcon = function(status)
-  {
-    switch (status)
-    {
-      case 0:
-        return '/media/images/accept.png';
-      case 1:
-        return '/media/images/bell.png';
-    }
-  };
-
-Dashboard.Sip.prototype.build = function()
-  {
-    this.$object = $('<div class="sip" />').attr({'uuid': this.uuid, 'status': this.status});
+    window.Job = Backbone.Model.extend({
+    });
     
-    if (this.status)
-    {
-      this.$object.addClass('sip-highlighted');
-    }
+    window.JobCollection = Backbone.Collection.extend({
+    
+      model: Job,
+    
+    });
 
-    this.$object.append('<div class="sip-detail sip-detail-icon"><img src="' + this.getIcon(this.status) + '" /></div>');
-    this.$object.append('<div class="sip-detail sip-detail-directory">' + this.directory + '</div>');
-    this.$object.append('<div class="sip-detail sip-detail-uuid">' + this.uuid + '</div>');
-    this.$object.append('<div class="sip-detail sip-detail-timestamp">' + this.timestamp + '</div>');
-    this.$object.append('<div class="sip-detail sip-detail-jobs"><span>Show jobs</span></div>');
-  };
+    window.JobView = Backbone.View.extend({
+    
+      className: 'job',
+      
+      template: _.template($('#job-template').html()),
 
-Dashboard.Sip.prototype.render = function()
-  {
-    this.$container.append(this.$object);
-  };
-
-Dashboard.Sip.prototype.replace = function(sip)
-  {
-    if (this.timestamp != sip.timestamp)
-    {
-      this.load(sip);
-      this.build();
-
-      $('div.sip[uuid=' + this.uuid + ']', this.$container).replaceWith(this.$object);
-
-      this.$object.addClass('sip-reloaded');
-      var self = this;
-      setTimeout(function()
+      initialize: function()
         {
-          self.$object.removeClass('sip-reloaded');
-        }, 500);
-    }
-  };
+          _.bindAll(this, 'render');
+          this.model.view = this;
+        },
+
+      render: function()
+        {
+          $(this.el).html(this.template(this.model.toJSON()));
+
+          return this;
+        },
+
+    });
+    
+    window.AppView = Backbone.View.extend({
+    
+      el: $('#sip-container'),
+
+      initialize: function()
+        {
+          _.bindAll(this, 'addOne', 'addAll');
+          Sips.bind('refresh', this.addAll);
+          Sips.fetch();
+        },
+      
+      addOne: function(sip)
+        {
+          var view = new SipView({model: sip});
+
+          var $current = this.el.find('.sip[uuid=' + sip.get('uuid') + ']');
+          if ($current.length)
+          {
+            if ($current.hasClass('sip-selected'))
+            {
+              
+            }
+            else
+            {
+              $current.replaceWith(view.render().el);
+            }
+          }
+          else
+          {
+            this.el.children('#sip-body').append(view.render().el);
+          }
+        },
+
+      addAll: function()
+        {
+          Sips.each(this.addOne);
+
+          setTimeout(function()
+            {
+              Sips.fetch();
+            }, window.pollingInterval ? window.pollingInterval * 1000: 5000);
+        }
+
+    });
+
+  }
+);
