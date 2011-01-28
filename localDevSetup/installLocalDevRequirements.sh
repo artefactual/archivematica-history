2bcdf8724d578fad25e99f3dfec5b7fcbb1e73c4
Joseph Perry <berwin22@gmail.com>
2011-01-28 21:23:59 +0000

    Modified to detect Lucid and automatically do the ppa add.
    Autoconverted from SVN (revision:1063)

--- localDevSetup/installLocalDevRequirements.sh
+++ localDevSetup/installLocalDevRequirements.sh
@@ -1,12 +1,19 @@
 sudo apt-get install libapache2-mod-wsgi python-django python-django-doc python-twisted python-pyinotify ffmpeg2theora libavcodec-unstripped-52 flashplugin-installer openjdk-6-jre openoffice.org openoffice.org-java-common libnotify-bin uuid httrack sendemail curl clamav incron nfs-common flac md5deep ffmpeg winff firebug imagemagick libapache2-mod-php5 mysql-server php5-cli php5-mysql php5-xsl subversion-tools par2 unrar p7zip-full python-execnet digikam kipi-plugins python-mysqldb nfs-kernel-server python-lxml inkscape dosfstools
-##LUCID
-#sudo add-apt-repository ppa:austin-arcintel/archivematica
-##OTHERS
-#/etc/apt/sources.list
-#deb http://ppa.launchpad.net/austin-arcintel/archivematica/ubuntu lucid main
-#deb-src http://ppa.launchpad.net/austin-arcintel/archivematica/ubuntu lucid main 
-sudo apt-get update
 
+
+
+version="`lsb_release -d | grep "Ubuntu 10.04"`"
+if [ -n "$version" ]; 
+	then
+		##LUCID
+		sudo add-apt-repository ppa:austin-arcintel/archivematica
+	
+	else
+		##OTHERS
+		sudo echo deb http://ppa.launchpad.net/austin-arcintel/archivematica/ubuntu lucid main >> /etc/apt/sources.list
+		sudo echo deb-src http://ppa.launchpad.net/austin-arcintel/archivematica/ubuntu lucid main >> /etc/apt/sources.list 
+fi
+sudo apt-get update
 sudo apt-get install bagit droid fits jhove # xena 
 
 tmp="`pwd`"
