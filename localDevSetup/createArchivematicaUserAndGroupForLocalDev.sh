02eff927999dbf603c2a9366ef6f4320a5b133db
Austin Trask <austin.trask@gmail.com>
2011-02-01 18:52:03 +0000

    removed no-create-home and nologin directives for addarchivematicaUser script
    Autoconverted from SVN (revision:1116)

--- localDevSetup/createArchivematicaUserAndGroupForLocalDev.sh
+++ localDevSetup/createArchivematicaUserAndGroupForLocalDev.sh
@@ -1,6 +1,6 @@
 #Create archivematica User for daemons, add demo user to group
 ##!!!Some of this belongs in the installer for the MCP client & server
-sudo adduser --uid 333 --group --system --no-create-home --disabled-login archivematica
+sudo adduser --uid 333 --group --system --home /var/archivematica/home/ archivematica
 echo -n " Your username? : "
 read username
 sudo gpasswd -a $username archivematica
