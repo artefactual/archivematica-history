5758e81cfd0884f0ac49e40b0815697b84dd6b2c
Jesús García Crespo <jesus@sevein.com>
2011-02-03 22:03:29 +0000

    JavaScript should print time in local time, not UTC. See issue 415.
    Autoconverted from SVN (revision:1137)

--- src/dashboard/src/media/js/sips.js
+++ src/dashboard/src/media/js/sips.js
@@ -688,7 +688,7 @@ $(function()
             return n < 10 ? '0' + n : n;
           }
 
-        return this.getUTCFullYear() + '-' + pad(this.getUTCMonth() + 1) + '-' + pad(this.getUTCDate()) + ' ' + pad(this.getUTCHours()) + ':' + pad(this.getUTCMinutes()); // + ':' + pad(this.getUTCSeconds());
+        return this.getFullYear() + '-' + pad(this.getMonth() + 1) + '-' + pad(this.getDate()) + ' ' + pad(this.getHours()) + ':' + pad(this.getMinutes()); // + ':' + pad(this.getSeconds());
       };
   }
 );
