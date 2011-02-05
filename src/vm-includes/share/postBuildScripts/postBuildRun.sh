6ede573ccfb0b9b49801036c37bc110c92269310
Austin Trask <austin.trask@gmail.com>
2011-02-05 19:57:38 +0000

    added vm-includes package to src tree
    Autoconverted from SVN (revision:1159)

---
+++ src/vm-includes/share/postBuildScripts/postBuildRun.sh
@@ -0,0 +1,30 @@
+echo "The default password is demo"
+#cd postBuildRunAssistScripts
+#./preMCPLogging.sh
+sudo mysqladmin create ica-atom
+sudo mysqladmin create dcb
+sudo mysqladmin create qubit
+sudo mysqladmin create dashboard
+
+sudo aptitude install archivematica-shotgun
+
+sudo cp apache.default /etc/apache2/sites-available/default
+#xfce4 configuration
+cp ./xfceCustomization/gtk-bookmarks /home/demo/.gtk-bookmarks
+
+#fix permissions 
+chmod 444 /home/demo/.config/xfce4/panel
+chown -R demo:demo /home/demo
+chown -R demo:demo /home/demo/.mozilla
+
+sudo chmod 444 -R ~/.config/xfce4/panel
+sudo chmod 770 -R  /var/archivematica/sharedDirectory/
+sudo chown -R archivematica:archivematica /var/archivematica/sharedDirectory/
+sudo chmod -R g+s /var/archivematica/sharedDirectory/
+echo "Disabling Screen Saver (Better for VM's)"
+sudo aptitude remove xscreensaver
+sudo gpasswd -a demo archivematica
+echo " "
+echo "===PLEASE REBOOT TO ENABLE NEW GROUP SETTINGS==="
+echo " "
+sleep 3
---
+++ src/vm-includes/share/vmInstaller-enviroment.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# This file is part of Archivematica.
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# @package Archivematica
+# @subpackage Configuration
+# @author Austin Trask <austin@artefactual.com>
+# @author Joseph Perry <joseph@artefactual.com>
+# @version svn: $Id$
+
+
+#Create archivematica User for daemons, add demo user to group
+##!!!Some of this belongs in the installer for the MCP client & server
+#chroot "$1" adduser --uid 333 --group --system --no-create-home --disabled-login archivematica
+#chroot "$1" sudo gpasswd -a demo archivematica
+#chroot "$1" update-python-modules
+
+#Configure sudoers for mcp and client
+#echo "archivematica ALL=NOPASSWD:/bin/mv,/bin/chown,/bin/chmod" >> $1/etc/sudoers
+
+#Install externals/archivematica
+chroot "$1" mkdir -p /home/demo/Desktop
+svn export ./.mozilla $1/home/demo/.mozilla
+svn export ./Docs $1/home/demo/Docs
+svn export enviromentConfigFiles/exports $1/etc/exports
+chroot "$1" ln -s /home/demo/Docs /home/demo/Desktop
+chroot "$1" chmod -R 770 /home/demo/.mozilla
+
+#setup unonconv service
+#cp -rf ./unoconv-listen $1/etc/init.d/unoconv-listen
+#chroot "$1" chmod +x /etc/init.d/unoconv-listen
+#chroot "$1" /usr/sbin/update-rc.d unoconv-listen defaults
+#chroot "$1" mkdir -p /home/demo/.gnome2/nautilus-scripts
+
+svn export ./sampledata $1/home/demo/testFiles
+svn export ./postBuildScripts $1/home/demo/postBuildScripts
+
+#XFCE configuration
+chroot "$1" mkdir /home/demo/.config
+chroot "$1" mkdir /home/demo/.config/Thunar
+chroot "$1" mkdir /home/demo/.config/autostart
+chroot "$1" mkdir -p /home/demo/.config/xfce4/desktop
+chroot "$1" mkdir -p /home/demo/.config/xfce4/panel
+
+#add archivematica/dashboard icons
+cp ./desktopShortcuts/dashboard-desktop-icon.png $1/usr/share/icons
+cp ./desktopShortcuts/dcb-desktop-icon.png $1/usr/share/icons
+cp ./desktopShortcuts/ica-atom-desktop-icon.png $1/usr/share/icons
+cp ./desktopShortcuts/archivematica-xubuntu-steel.png $1/usr/share/xfce4/backdrops/xubuntu-karmic.png
+cp ./desktopShortcuts/ica-atom.desktop $1/home/demo/Desktop
+#cp ./droid.desktop $1/home/demo/Desktop
+#cp ./jhove.desktop $1/home/demo/Desktop
+cp ./desktopShortcuts/dcb.desktop $1/home/demo/Desktop
+cp ./dashboard.desktop $1/home/demo/Desktop
+#cp ./WinFF.desktop $1/home/demo/Desktop
+cp ./desktopShortcuts/Terminal.desktop $1/home/demo/Desktop
+
+#add launcher scripts
+cp ./desktopShortcuts/runica-atom.sh $1/usr/bin
+cp ./desktopShortcuts/rundcb.sh $1/usr/bin
+cp ./desktopShortcuts/rundashboard.sh $1/usr/bin
+#cp ./desktopShortcuts/runjhove.sh $1/usr/bin
+
+#xfce4 configuration
+cp ./panel/* $1/home/demo/.config/xfce4/panel
+cp ./xfceCustomization/xfce4-desktop.xml $1/etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/
+cp ./xfceCustomization/xfce4-session.xml $1/etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml/
+cp ./xfceCustomization/icons.screen0.rc $1/home/demo/.config/xfce4/desktop
+cp ./xfceCustomization/user-dirs.defaults $1/etc/xdg
+cp ./xfceCustomization/uca.xml $1/home/demo/.config/Thunar
+cp ./xfceCustomization/thunarrc $1/home/demo/.config/Thunar
+cp ./xfceCustomization/thunar.desktop $1/home/demo/.config/autostart
+#cp ./xfceCustomization/gtk-bookmarks $1/home/demo/.gtk-bookmarks
+cp ./xfceCustomization/gdm.custom.conf $1/etc/gdm/custom.conf
+
+#fix permissions 
+chroot "$1" chmod 444 /home/demo/.config/xfce4/panel
+chroot "$1" chown -R demo:demo /home/demo
+chroot "$1" chown -R demo:demo /home/demo/.mozilla
+
+
