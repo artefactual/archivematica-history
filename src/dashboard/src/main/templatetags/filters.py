89c4f8243cfa69bb82494fb8b72541e8db27d082
Jesús García Crespo <jesus@sevein.com>
2011-02-05 23:17:16 +0000

    Remove unused custom filters.
    Autoconverted from SVN (revision:1166)

--- src/dashboard/src/main/templatetags/filters.py
+++ src/dashboard/src/main/templatetags/filters.py
@@ -2,15 +2,3 @@ from django import template
 import os
 
 register = template.Library()
-
-@register.filter('join_path')
-def join_path(value, path):
-  return os.path.join(value, path)
-
-@register.filter('is_file')
-def is_file(value, basedir):
-  return os.path.isfile(os.path.join(basedir, value))
-
-@register.filter('is_dir')
-def is_dir(value, basedir):
-  return os.path.isdir(os.path.join(basedir, value))
