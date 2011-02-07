2223eb2a3eec17f8dddd1fd59ca821ecba73845c
Austin Trask <austin.trask@gmail.com>
2011-02-07 08:10:27 +0000

    fixed path for apache.default and php.ini
    Autoconverted from SVN (revision:1188)

--- src/vm-includes/share/vmInstaller-enviroment.sh
+++ src/vm-includes/share/vmInstaller-enviroment.sh
@@ -49,9 +49,9 @@ mysqladmin create dashboard $dpPassword
 
 dpPassword=""
 
-cp php.ini /etc/php5/cli
-cp php.ini /etc/php5/apache2
-cp apache.default /etc/apache2/sites-available/default
+cp ${includesDir}php.ini /etc/php5/cli
+cp ${includesDir}php.ini /etc/php5/apache2
+cp ${includesDir}apache.default /etc/apache2/sites-available/default
 
 
 
