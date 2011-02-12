3629319d5eb5fac0312a3643ee91683b9b8d8e6a
Austin Trask <austin.trask@gmail.com>
2011-02-12 21:13:39 +0000

    updated dashboard icon/link
    Autoconverted from SVN (revision:1208)

--- src/vm-includes/share/desktopShortcuts/rundashboard.sh
+++ src/vm-includes/share/desktopShortcuts/rundashboard.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ~/.mozilla -name extensions.cache -exec rm -f {} \; && firefox http://localhost/dashboard
+find ~/.mozilla -name extensions.cache -exec rm -f {} \; && firefox http://localhost/sips
