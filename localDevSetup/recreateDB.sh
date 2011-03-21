1dacacec6b10282d3015266ad109d5f28b6f4b3e
Joseph Perry <berwin22@gmail.com>
2011-03-21 18:03:34 +0000

    In relation to Issue 446: 	Compile METS micro-service fails if video normalization fails. Checks for size 0 files and errors if output file is 0 bytes.
    Autoconverted from SVN (revision:1246)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -1,4 +1,5 @@
 databaseName="MCP"
+echo "Enter root database password twice. (Hit enter if blank)"
 sudo mysql -p --execute="source ../src/MCPServer/share/mysql" "$databaseName"
 sudo mysql -p --execute="source ../src/transcoder/share/mysql" "$databaseName"
 
