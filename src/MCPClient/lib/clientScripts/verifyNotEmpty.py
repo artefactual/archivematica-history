6b997288cbb189838c015a07760657f53df1cb25
Joseph Perry <berwin22@gmail.com>
2011-04-28 00:30:09 +0000

    Added test to ensure there is submission documentation before trying to process it. MCP will stop processing a sip, and leave it in the processing directory, if it tries processing an emty directory (it seems).
    Autoconverted from SVN (revision:1346)

---
+++ src/MCPClient/lib/clientScripts/verifyNotEmpty.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# This file is part of Archivematica.
+#
+# Copyright 2010-2011 Artefactual Systems Inc. <http://artefactual.com>
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+# @package Archivematica
+# @subpackage Ingest
+# @author Joseph Perry <joseph@artefactual.com>
+# @version svn: $Id$
+
+import os
+import sys
+target = sys.argv[1]
+if not os.path.isdir(target):
+    print >>sys.stderr, "Directory doesn't exist: ", target
+    exit(1)
+if os.listdir(target) == []:
+    print >>sys.stderr, "Directory is empty: ", target
+    exit(1)
+else:
+    exit(0)
+      
