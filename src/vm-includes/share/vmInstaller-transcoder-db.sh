afda4a1462302480a44d1d12d4d505314a2b6f95
Austin Trask <austin.trask@gmail.com>
2011-04-29 06:47:09 +0000

    updated path to transcoder db in vmInstaller-transcover-db.sh
    Autoconverted from SVN (revision:1349)

--- src/vm-includes/share/vmInstaller-transcoder-db.sh
+++ src/vm-includes/share/vmInstaller-transcoder-db.sh
@@ -4,4 +4,4 @@ databaseName="MCP"
 username="demo"
 dpPassword="$1"
 
-sudo mysql $dpPassword --execute="source /usr/share/transcoder/mysql" "$databaseName"
+sudo mysql $dpPassword --execute="source /usr/share/archivematica/transcoder/mysql" "$databaseName"
