fd24d1168a6788210a4f26a3423e925c82b2dd5a
Jesús García Crespo <jesus@sevein.com>
2011-04-29 14:07:21 +0000

    Cosmetic changes.
    Autoconverted from SVN (revision:1350)

--- src/dashboard/src/main/urls.py
+++ src/dashboard/src/main/urls.py
@@ -5,16 +5,10 @@ from django.views.generic.simple import direct_to_template, redirect_to
 UUID_REGEX = '[\w]{8}(-[\w]{4}){3}-[\w]{12}'
 
 urlpatterns = patterns('dashboard.main.views',
-
-  (r'ingest/$', direct_to_template, {'template': 'main/ingest.html', 'extra_context': {'polling_interval': settings.POLLING_INTERVAL, 'microservices_help': settings.MICROSERVICES_HELP}}, 'sips'),
-
+  (r'ingest/$', direct_to_template, {'template': 'main/ingest.html', 'extra_context': {'polling_interval': settings.POLLING_INTERVAL, 'microservices_help': settings.MICROSERVICES_HELP}}),
   (r'ingest/go/$', 'ingest'),
-
   (r'ingest/go/(?P<uuid>' + UUID_REGEX + ')$', 'ingest'),
-
   (r'jobs/(?P<uuid>' + UUID_REGEX + ')/explore/$', 'explore'),
-
   (r'tasks/(?P<uuid>' + UUID_REGEX + ')/$', 'tasks'),
-
   (r'', redirect_to, {'url': '/ingest/'}),
 )
