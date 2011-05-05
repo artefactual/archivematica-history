87232da1aaf97c21103022d7087deb9d65b08b75
Joseph Perry <berwin22@gmail.com>
2011-05-05 01:43:49 +0000

    Related to: Issue 558:	Submission documentation micro-service fails if user does not add documents to submissionDocumentation folder
    Autoconverted from SVN (revision:1366)

--- src/MCPClient/lib/clientScripts/verifyNotEmpty.py
+++ src/MCPClient/lib/clientScripts/verifyNotEmpty.py
@@ -27,10 +27,14 @@ import sys
 target = sys.argv[1]
 if not os.path.isdir(target):
     print >>sys.stderr, "Directory doesn't exist: ", target
-    exit(1)
+    os.mkdir(target)
 if os.listdir(target) == []:
     print >>sys.stderr, "Directory is empty: ", target
-    exit(1)
+    fileName = os.path.join(target, "submission documentation.log")
+    f = open(fileName, 'a')
+    f.write("No submission documentation added")
+    f.close()
+    os.chmod(fileName, 488)
 else:
     exit(0)
       
