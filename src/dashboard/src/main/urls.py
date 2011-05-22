6367c0123c15e2486b877408d3082935be27963d
Jesús García Crespo <jesus@sevein.com>
2011-05-22 03:17:08 +0000

    Fix ingest link from menu, using named url patterns.
    Autoconverted from SVN (revision:1403)

--- src/dashboard/src/main/urls.py
+++ src/dashboard/src/main/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template, redirect_to
 UUID_REGEX = '[\w]{8}(-[\w]{4}){3}-[\w]{12}'
 
 urlpatterns = patterns('dashboard.main.views',
-  (r'ingest/$', direct_to_template, {'template': 'main/ingest.html', 'extra_context': {'polling_interval': settings.POLLING_INTERVAL, 'microservices_help': settings.MICROSERVICES_HELP}}),
+  url(r'ingest/$', direct_to_template, {'template': 'main/ingest.html', 'extra_context': {'polling_interval': settings.POLLING_INTERVAL, 'microservices_help': settings.MICROSERVICES_HELP}}, 'ingest'),
   (r'ingest/go/$', 'ingest'),
   (r'ingest/go/(?P<uuid>' + UUID_REGEX + ')$', 'ingest'),
   (r'jobs/(?P<uuid>' + UUID_REGEX + ')/explore/$', 'explore'),
