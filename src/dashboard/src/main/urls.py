d95eda2f88a8b493e40cd6628c7e532a1f510610
Jesús García Crespo <jesus@sevein.com>
2011-05-23 21:53:12 +0000

    Remove default route because it is not the desired behavior.
    Autoconverted from SVN (revision:1409)

--- src/dashboard/src/main/urls.py
+++ src/dashboard/src/main/urls.py
@@ -6,17 +6,18 @@ UUID_REGEX = '[\w]{8}(-[\w]{4}){3}-[\w]{12}'
 
 urlpatterns = patterns('dashboard.main.views',
 
+  # Index
+  (r'^$', redirect_to, {'url': '/ingest/'}),
+
   # Ingest
   url(r'ingest/$', direct_to_template, {'template': 'main/ingest.html', 'extra_context': {'polling_interval': settings.POLLING_INTERVAL, 'microservices_help': settings.MICROSERVICES_HELP}}, 'ingest'),
   (r'ingest/go/$', 'ingest'),
   (r'ingest/go/(?P<uuid>' + UUID_REGEX + ')$', 'ingest'),
   (r'jobs/(?P<uuid>' + UUID_REGEX + ')/explore/$', 'explore'),
+  (r'jobs/(?P<uuid>' + UUID_REGEX + ')/list-objects/$', 'list_objects'),
   (r'tasks/(?P<uuid>' + UUID_REGEX + ')/$', 'tasks'),
 
   # Preservatin planning
   (r'preservation-planning/$', 'preservation_planning'),
-
-  # Index
-  (r'', redirect_to, {'url': '/ingest/'}),
 
 )
