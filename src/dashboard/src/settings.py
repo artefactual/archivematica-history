cdff1c1608c6b94c642e946476514a5281e6b05e
Jesús García Crespo <jesus@sevein.com>
2011-06-03 21:15:42 +0000

    Include Django 1.3 middleware and other changes in defaults for settings.py.
    Autoconverted from SVN (revision:1493)

--- src/dashboard/src/settings.py
+++ src/dashboard/src/settings.py
@@ -56,24 +56,22 @@ SECRET_KEY = 'e7b-$#-3fgu)j1k01)3tp@^e0=yv1hlcc4k-b6*ap^zezv2$48'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-  'django.template.loaders.filesystem.load_template_source',
-  'django.template.loaders.app_directories.load_template_source',
-  # 'django.template.loaders.eggs.load_template_source',
+  'django.template.loaders.filesystem.Loader',
+  # 'django.template.loaders.app_directories.Loader',
+  # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-  # 'django.middleware.common.CommonMiddleware',
+  'django.middleware.common.CommonMiddleware',
   # 'django.contrib.sessions.middleware.SessionMiddleware',
+  # 'django.middleware.csrf.CsrfViewMiddleware',
   # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+  # 'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'dashboard.urls'
 
 TEMPLATE_DIRS = (
-  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-  # Always use forward slashes, even on Windows.
-  # Don't forget to use absolute paths, not relative paths.
-
   os.path.join(BASE_PATH, 'templates'),
 )
 
@@ -84,6 +82,8 @@ INSTALLED_APPS = (
   # 'django.contrib.contenttypes',
   # 'django.contrib.sessions',
   # 'django.contrib.sites',
+  # 'django.contrib.messages',
+  # 'django.contrib.staticfiles',
 
   # Uncomment the next line to enable the admin:
   # 'django.contrib.admin',
@@ -91,7 +91,6 @@ INSTALLED_APPS = (
   # Internal apps
   'dashboard.main',
   'dashboard.mcp',
-
 )
 
 MCP_SERVER = ('127.0.0.1', 8000)
