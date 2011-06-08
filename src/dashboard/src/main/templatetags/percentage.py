22faee82e1f070532c0dfe5777136e842233a1f0
Joseph Perry <berwin22@gmail.com>
2011-06-08 21:57:22 +0000

    Fix % only showing 0 or 100%, everything between goes to 0%.
    Autoconverted from SVN (revision:1548)

--- src/dashboard/src/main/templatetags/percentage.py
+++ src/dashboard/src/main/templatetags/percentage.py
@@ -5,7 +5,7 @@ register = Library()
 @register.filter('percentage')
 def percentage(value, total):
   try:
-    percentage = int(value) / int(total) * 100
+    percentage = float(value) / float(total) * 100
   except ZeroDivisionError:
     percentage = 0
   return '<abbr title="%s/%s">%s%%</abbr>' % (value, total, percentage)
