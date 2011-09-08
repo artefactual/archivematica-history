641a95226abffa27c506d9a10cf1833a77d3a8b2
Jesús García Crespo <jesus@sevein.com>
2011-09-08 21:54:48 +0000

    Use current dir and get rid of sudo
    Autoconverted from SVN (revision:1692)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -1,7 +1,10 @@
 databaseName="MCP"
-sudo echo "Enter root database password for the following. (Hit enter if blank)"
-echo MCPServer
-sudo mysql -p --execute="source ../src/MCPServer/share/mysql" "$databaseName"
-echo transcoder
-sudo mysql -p --execute="source ../src/transcoder/share/mysql" "$databaseName"
+currentDir="$(dirname $0)"
 
+echo "Enter root database password for the following. (Hit enter if blank)"
+
+echo MCPServer
+mysql -u root -p --execute="source $currentDir/../src/MCPServer/share/mysql" "$databaseName"
+
+echo transcoder
+mysql -u root -p --execute="source $currentDir/../src/transcoder/share/mysql" "$databaseName"
