95d1b03f5d2f5bc0d9cc2525d59d4c5b5dafb607
Joseph Perry <berwin22@gmail.com>
2011-09-28 16:58:17 +0000

    Work on moving dspace mets files into %transfer%metadata/submissionDocumentation/DspaceMETS
    Autoconverted from SVN (revision:1721)

--- src/MCPClient/lib/clientScripts/moveDspaceMetsFilesToDSpaceMETS.py
+++ src/MCPClient/lib/clientScripts/moveDspaceMetsFilesToDSpaceMETS.py
@@ -30,36 +30,33 @@ from fileOperations import renameAsSudo
 
 def verifyMetsFileSecChecksums(metsFile, date, taskUUID, transferDirectory, transferUUID, relativeDirectory="./"):
     print metsFile
-    DspaceLicenses = "metadata/submissionDocumentation/DspaceLicenses"
+    DSpaceMets = "metadata/submissionDocumentation/DSpaceMets"
     try:
-        path = os.path.join(transferDirectory, DspaceLicenses)
+        path = os.path.join(transferDirectory, DSpaceMets)
         if not os.path.isdir(path):
             os.mkdir(path)
     except:
-        print "error creating DspaceLicenses directory."
+        print "error creating DSpaceMets directory."
     exitCode = 0
-    tree = etree.parse(metsFile)
-    root = tree.getroot()
-    for item in root.findall("{http://www.loc.gov/METS/}fileSec/{http://www.loc.gov/METS/}fileGrp"):
-        #print etree.tostring(item)
-        #print item
-        
-        USE = item.get("USE")
-        if USE == "LICENSE":
-            for item2 in item:
-                if item2.tag == "{http://www.loc.gov/METS/}file":
-                    for item3 in item2:
-                        if item3.tag == "{http://www.loc.gov/METS/}FLocat":
-                            fileLocation = item3.get("{http://www.w3.org/1999/xlink}href")
-                            fileFullPath = os.path.join(relativeDirectory, fileLocation)
-                            dest = os.path.join(transferDirectory, DspaceLicenses, os.path.basename(fileLocation))
-                            renameAsSudo(fileFullPath, dest)
-                            
-                            src = fileFullPath.replace(transferDirectory, "%transferDirectory%") 
-                            dst = dest.replace(transferDirectory, "%transferDirectory%")
-                            eventDetail = ""
-                            eventOutcomeDetailNote = "moved from=\"" + src + "\"; moved to=\"" + dst + "\""
-                            updateFileLocation(src, dst, "movement", date, eventDetail, transferUUID=transferUUID, eventOutcomeDetailNote = eventOutcomeDetailNote)
+    
+    metsDirectory = os.path.basename(os.path.dirname(metsFile))
+    
+    if metsDirectory == "DSpace_export":
+        outputDirectory = path
+    else:
+        outputDirectory = os.path.join(path, metsDirectory)
+        if not os.path.isdir(outputDirectory):
+            os.mkdir(outputDirectory)
+    
+    dest = os.path.join(outputDirectory, "mets.xml")
+    renameAsSudo(metsFile, dest)
+                        
+    src = metsFile.replace(transferDirectory, "%transferDirectory%") 
+    dst = dest.replace(transferDirectory, "%transferDirectory%")
+    eventDetail = ""
+    eventOutcomeDetailNote = "moved from=\"" + src + "\"; moved to=\"" + dst + "\""
+    updateFileLocation(src, dst, "movement", date, eventDetail, transferUUID=transferUUID, eventOutcomeDetailNote = eventOutcomeDetailNote)                            
+
     return exitCode
              
 
