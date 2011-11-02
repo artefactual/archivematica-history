39163fa128b114f95ff5c22a8ee1de84b9bcc0cc
Jesús García Crespo <jesus@sevein.com>
2011-11-02 06:15:19 +0000

    HTTP response codes class
    Autoconverted from SVN (revision:1810)

---
+++ src/upload-qubit/lib/HttpResponseCodes.class.php
@@ -0,0 +1,67 @@
+<?php
+
+# This file is part of Archivematica.
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+class HttpResponseCodes
+{
+  private static $messages = array(
+    100 => '100 Continue',
+    101 => '101 Switching Protocols',
+    200 => '200 OK',
+    201 => '201 Created',
+    202 => '202 Accepted',
+    203 => '203 Non-Authoritative Information',
+    204 => '204 No Content',
+    205 => '205 Reset Content',
+    206 => '206 Partial Content',
+    300 => '300 Multiple Choices',
+    301 => '301 Moved Permanently',
+    302 => '302 Found',
+    303 => '303 See Other',
+    304 => '304 Not Modified',
+    305 => '305 Use Proxy',
+    306 => '306 (Unused)',
+    307 => '307 Temporary Redirect',
+    400 => '400 Bad Request',
+    401 => '401 Unauthorized',
+    402 => '402 Payment Required',
+    403 => '403 Forbidden',
+    404 => '404 Not Found',
+    405 => '405 Method Not Allowed',
+    406 => '406 Not Acceptable',
+    407 => '407 Proxy Authentication Required',
+    408 => '408 Request Timeout',
+    409 => '409 Conflict',
+    410 => '410 Gone',
+    411 => '411 Length Required',
+    412 => '412 Precondition Failed',
+    413 => '413 Request Entity Too Large',
+    414 => '414 Request-URI Too Long',
+    415 => '415 Unsupported Media Type',
+    416 => '416 Requested Range Not Satisfiable',
+    417 => '417 Expectation Failed',
+    500 => '500 Internal Server Error',
+    501 => '501 Not Implemented',
+    502 => '502 Bad Gateway',
+    503 => '503 Service Unavailable',
+    504 => '504 Gateway Timeout',
+    505 => '505 HTTP Version Not Supported');
+
+  public static function getMessage($code)
+  {
+    return self::$messages[$code];
+  }
+}
--- src/upload-qubit/lib/upload-qubit.php
+++ src/upload-qubit/lib/upload-qubit.php
@@ -1,6 +1,21 @@
 #!/usr/bin/env php
 <?php
 
+# This file is part of Archivematica.
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
 error_reporting(E_ALL ^ E_NOTICE);
 
 function zip ($source, $destination)
@@ -118,12 +133,17 @@ catch (Exception $e)
 
   if (isset($e->data['status']))
   {
-    fwrite(STDERR, "[!!] HTTP response status code: " . $e->data['status'] . "\n");
+    require(dirname(__FILE__).'/HttpResponseCodes.class.php');
+    fwrite(STDERR, '[!!] HTTP response status code: ' . HttpResponseCodes::getMessage($e->data['status']) . "\n");
 
     if ('404' == $e->data['status'])
     {
       fwrite(STDERR, "[!!] Please check that qtSwordPlugin is enabled.\n");
       fwrite(STDERR, "[!!] Please check that the next resource exists: " . $cfg['url'] . "\n");
+    }
+    else if ('401' == $e->data['status'])
+    {
+      fwrite(STDERR, "[!!] Bad username/password?\n");
     }
   }
 
