7452ee88989de297939fe34e97c1ab7aea66cd5c
Joseph Perry <berwin22@gmail.com>
2011-11-29 22:36:01 +0000

    Issue 824:	unable to verify checksums that come with SIPs.
    Autoconverted from SVN (revision:1946)

--- src/MCPClient/lib/clientScripts/verifyMD5.sh
+++ src/MCPClient/lib/clientScripts/verifyMD5.sh
@@ -28,6 +28,7 @@ target="$1"
 checksums="$2"
 date="$3"
 eventID="$4"
+transferUUID="$5"
 
 
 #       md5deep - Compute and compare MD5 message digests
@@ -37,10 +38,11 @@ eventID="$4"
 #       whirlpooldeep - Compute and compare Whirlpool message digests
 
 ret=0
+
 MD5FILE="${target}metadata/${checksums}.md5"
 if [ -f "${MD5FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${MD5FILE}" "${target}logs/`basename "${MD5FILE}"`-Check-`date`" "md5deep" && \
-    "`dirname $0`/createXMLeventsMD5Verified.py" "$eventID" "$date" "`md5deep -v`" "md5deep" "${target}"  
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`md5deep -v` md5deep ${target}"   
     ret+="$?"
 else
     echo "File Does not exist:" "${MD5FILE}"
@@ -49,7 +51,7 @@ fi
 SHA1FILE="${target}metadata/${checksums}.sha1"
 if [ -f "${SHA1FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${SHA1FILE}" "${target}logs/`basename "${SHA1FILE}"`-Check-`date`" "sha1deep" && \
-    "`dirname $0`/createXMLeventsMD5Verified.py" "$eventID" "$date" "`sha1deep -v`" "sha1deep" "${target}"  
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha1deep -v` sha1deep ${target}"  
     ret+="$?"
 else
     echo "File Does not exist:" "${SHA1FILE}"
@@ -58,7 +60,7 @@ fi
 SHA256FILE="${target}metadata/${checksums}.sha256"
 if [ -f "${SHA256FILE}" ]; then
     "${checkMD5NoGui}" "${target}objects/" "${SHA256FILE}" "${target}logs/`basename "${SHA256FILE}"`-Check-`date`" "sha256deep" && \
-    "`dirname $0`/createXMLeventsMD5Verified.py" "$eventID" "$date" "`sha256deep -v`" "sha256deep" "${target}"  
+    "`dirname $0`/createEventsForGroup.py" --groupUUID "${transferUUID}" --groupType "transferUUID" --eventType "fixity" --eventDateTime "$date" --eventOutcome "Pass" --eventDetail "`sha256deep -v` sha256deep ${target}"  
     ret+="$?"
 else
     echo "File Does not exist:" "${SHA256FILE}"
