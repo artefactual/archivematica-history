dafa1a114f8d59199abe7108c7c1a7761effab71
Jesús García Crespo <jesus@sevein.com>
2011-12-15 19:07:10 +0000

    Set utf8 character set in the database in recreateDB.sh
    Autoconverted from SVN (revision:2081)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -6,8 +6,6 @@ sudo ./removeUnitsFromWatchedDirectories.py
 
 echo "Enter root database password for each the following. (Hit enter if blank)"
 
-# CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci
-mysql -u root -p --execute="DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP;" \
+mysql -u root -p --execute="DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;" \
                  --execute="USE $databaseName; source $currentDir/../src/MCPServer/share/mysql;" \
                  --execute="USE $databaseName; source $currentDir/../src/transcoder/share/mysql;"
-
