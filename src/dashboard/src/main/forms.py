9ec664e6bc91199286245d829a448727f791d28e
Jesús García Crespo <jesus@sevein.com>
2011-12-22 22:07:29 +0000

    Cosmetic changes
    Autoconverted from SVN (revision:2152)

--- src/dashboard/src/main/forms.py
+++ src/dashboard/src/main/forms.py
@@ -36,32 +36,45 @@ class SettingsForm(forms.Form):
 
 class RightsForm(ModelForm):
     rightsbasis = forms.ChoiceField(choices=(
-      ('Copyright', 'Copyright'),
-      ('Statute', 'Statute'),
-      ('License', 'License'),
-      ('Policy', 'Policy'),
-      ('Donor', 'Donor')
+        ('Copyright', 'Copyright'),
+        ('Statute', 'Statute'),
+        ('License', 'License'),
+        ('Policy', 'Policy'),
+        ('Donor', 'Donor')
     ))
 
     class Meta:
         model = models.RightsStatement
 
-        exclude = ('id', 'metadataappliestotype', 'metadataappliestoidentifier', 'rightsstatementidentifier', 'rightsstatementidentifiertype', 'rightsstatementidentifiervalue',)
+        exclude = (
+            'id',
+            'metadataappliestotype',
+            'metadataappliestoidentifier',
+            'rightsstatementidentifier',
+            'rightsstatementidentifiertype',
+            'rightsstatementidentifiervalue',)
         widgets = {
-            'rightsstatementidentifier': TextInput(attrs=INPUT_ATTRS), 'rightsnotes': Textarea(attrs=TEXTAREA_ATTRS), 'rightsholder': TextInput(attrs=INPUT_ATTRS), }
+            'rightsstatementidentifier': TextInput(attrs=INPUT_ATTRS),
+            'rightsnotes': Textarea(attrs=TEXTAREA_ATTRS),
+            'rightsholder': TextInput(attrs=INPUT_ATTRS), }
 
 class RightsGrantedForm(ModelForm):
     class Meta:
         model = models.RightsStatementRightsGranted
 
         widgets = {
-            'act': TextInput(attrs=INPUT_ATTRS), 'restriction': TextInput(attrs=INPUT_ATTRS), 'startdate': TextInput(attrs=INPUT_ATTRS), 'enddate': TextInput(attrs=INPUT_ATTRS), }
+            'act': TextInput(attrs=INPUT_ATTRS),
+            'restriction': TextInput(attrs=INPUT_ATTRS),
+            'startdate': TextInput(attrs=INPUT_ATTRS),
+            'enddate': TextInput(attrs=INPUT_ATTRS), }
 
 class RightsCopyrightForm(ModelForm):
     class Meta:
         model = models.RightsStatementCopyright
         widgets = {
-            'copyrightstatus': TextInput(attrs=INPUT_ATTRS), 'copyrightjurisdiction': TextInput(attrs=INPUT_ATTRS), 'copyrightstatusdeterminationdate': TextInput(attrs=INPUT_ATTRS), }
+            'copyrightstatus': TextInput(attrs=INPUT_ATTRS),
+            'copyrightjurisdiction': TextInput(attrs=INPUT_ATTRS),
+            'copyrightstatusdeterminationdate': TextInput(attrs=INPUT_ATTRS), }
 
 class RightsCopyrightNoteForm(ModelForm):
     class Meta:
@@ -73,13 +86,16 @@ class RightsStatuteForm(ModelForm):
     class Meta:
         model = models.RightsStatementStatuteInformation
         widgets = {
-            'statutejurisdiction': TextInput(attrs=INPUT_ATTRS), 'statutecitation': TextInput(attrs=INPUT_ATTRS), 'statutedeterminationdate': TextInput(attrs=INPUT_ATTRS), }
+            'statutejurisdiction': TextInput(attrs=INPUT_ATTRS),
+            'statutecitation': TextInput(attrs=INPUT_ATTRS),
+            'statutedeterminationdate': TextInput(attrs=INPUT_ATTRS), }
 
 class RightsLicenseForm(ModelForm):
     class Meta:
         model = models.RightsStatementLicense
         widgets = {
-            'licenseidentifier': TextInput(attrs=INPUT_ATTRS), 'licenseterms': TextInput(attrs=INPUT_ATTRS), }
+            'licenseidentifier': TextInput(attrs=INPUT_ATTRS),
+            'licenseterms': TextInput(attrs=INPUT_ATTRS), }
 
 class RightsLicenseNoteForm(ModelForm):
     class Meta:
