aed5e8247f615e99a333b8c1b3703ac033fc89cd
Jesús García Crespo <jesus@sevein.com>
2011-12-23 01:26:16 +0000

    Give to demo user SQL DELETE privs
    Autoconverted from SVN (revision:2171)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -8,4 +8,7 @@ echo "Enter root database password for each the following. (Hit enter if blank)"
 
 mysql -u root -p --execute="DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;" \
                  --execute="USE $databaseName; source $currentDir/../src/MCPServer/share/mysql;" \
-                 --execute="USE $databaseName; source $currentDir/../src/transcoder/share/mysql;"
+                 --execute="USE $databaseName; source $currentDir/../src/transcoder/share/mysql;" \
+                 --execute="DROP USER '${username}'@'localhost'" \
+                 --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'" \
+                 --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost'"
--- src/MCPServer/share/preMCPLogging.sh
+++ src/MCPServer/share/preMCPLogging.sh
@@ -24,14 +24,9 @@ databaseName="MCP"
 username="demo"
 password="demo"
 
-# Specifying character settings in the database
-# http://dev.mysql.com/doc/refman/5.0/en/charset-applications.html
 mysql -hlocalhost -uroot --execute="DROP DATABASE IF EXISTS ${databaseName}"
 mysql -hlocalhost -uroot --execute="CREATE DATABASE ${databaseName} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
 mysql -hlocalhost -uroot --execute="source ./mysql" "$databaseName"
 mysql -hlocalhost -uroot --execute="DROP USER '${username}'@'localhost'"
 mysql -hlocalhost -uroot --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
-mysql -hlocalhost -uroot --execute="GRANT SELECT, UPDATE, INSERT ON ${databaseName}.* TO '${username}'@'localhost'"
-
-#to delete the database and all of it's contents
-# sudo mysqladmin drop MCP
+mysql -hlocalhost -uroot --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost'"
