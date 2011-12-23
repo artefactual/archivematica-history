891f55ddc751b1371a91f6f14c22c135ec493379
Jesús García Crespo <jesus@sevein.com>
2011-12-23 01:18:57 +0000

    Add DELETE
    Autoconverted from SVN (revision:2170)

--- localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
+++ localDevSetup/createDatabases/postBuildRunAssistScripts/preMCPLogging.sh
@@ -6,7 +6,7 @@ sudo mysqladmin create "$databaseName" $dpPassword
 #sudo mysql $databaseName
 sudo mysql $dpPassword --execute="source ../../../src/MCPServer/share/mysql" "$databaseName"
 sudo mysql $dpPassword --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'"
-sudo mysql $dpPassword --execute="GRANT SELECT, UPDATE, INSERT ON ${databaseName}.* TO '${username}'@'localhost'"
+sudo mysql $dpPassword --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost'"
 
 
 #to delete the database and all of it's contents
