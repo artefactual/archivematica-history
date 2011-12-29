32f06a7d3fc14600792a07bf00fab60af4ac395a
Jesús García Crespo <jesus@sevein.com>
2011-12-29 20:57:28 +0000

    Add wrapper for render_to_resposne to include tmpl context processors easily
    Autoconverted from SVN (revision:2231)

--- src/dashboard/src/contrib/utils.py
+++ src/dashboard/src/contrib/utils.py
@@ -1,3 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+def render(request, template, context={}):
+    return render_to_response(template, context, context_instance=RequestContext(request))
+
 def get_directory_name(job):
     """
       Expected format:
