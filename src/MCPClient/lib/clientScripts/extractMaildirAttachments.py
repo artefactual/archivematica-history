97d5dac7926db842a8c88c7a20161a2d47541f38
Joseph Perry <berwin22@gmail.com>
2012-05-18 22:39:31 +0000

    Splitting external and archivematica code for extracting maildir attachments.
    Autoconverted from SVN (revision:2551)

---
+++ src/MCPClient/lib/clientScripts/extractMaildirAttachments.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python -OO
+
+# This file is part of Archivematica.
+#
+# Copyright 2010-2012 Artefactual Systems Inc. <http://artefactual.com>
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+# @package Archivematica
+# @subpackage Ingest
+# @author Joseph Perry <joseph@artefactual.com>
+# @version svn: $Id$
+
+import mailbox
+import sys
+import os
+sys.path.append("/usr/lib/archivematica/archivematicaCommon")
+from externals.extractMaildirAttachments import parse
+
+   
+if __name__ == '__main__':
+    #http://www.doughellmann.com/PyMOTW/mailbox/
+    maildir = sys.argv[1]
+    import lxml.etree as etree
+    #print "Extracting attachments from: " + maildir
+    root = etree.Element("ArchivematicaMaildirAttachmentExtractionRecord")
+    root.set("directory", maildir) 
+    for maildirsub2 in os.listdir(maildir):
+        maildirsub = os.path.join(maildir, maildirsub2)
+        #print "Extracting attachments from: " + maildirsub
+        md = mailbox.Maildir(maildirsub)
+        directory = etree.SubElement(root, "subDir")
+        directory.set("dir", maildirsub2)
+        for item in md.iterkeys():
+            #print maildirsub2, item
+            fil = md.get_file(item)
+            out = parse(fil)
+            #for i in out.iterkeys():
+                #print i
+            if len(out['attachments']):
+                msg = etree.SubElement(directory, "msg")
+                etree.SubElement(msg, "Subject").text = out["subject"] 
+                etree.SubElement(msg, "Date").text = out['msgobj']['date']
+                etree.SubElement(msg, "To").text = out["to"]
+                etree.SubElement(msg, "From").text = out["from"]
+                for i in range(len(out['attachments'])):
+                    attachment = out['attachments'][i]
+                    #attachment = StringIO(file_data) TODO LOG TO FILE
+                    attch = etree.SubElement(msg, "attachment")
+                    etree.SubElement(attch, "name").text = attachment.name
+                    etree.SubElement(attch, "content_type").text = attachment.content_type
+                    etree.SubElement(attch, "size").text = str(attachment.size)
+                    etree.SubElement(attch, "create_date").text = attachment.create_date
+                    etree.SubElement(attch, "mod_date").text = attachment.mod_date
+                    etree.SubElement(attch, "read_date").text = attachment.read_date
+    print etree.tostring(root, pretty_print=True)
+
+                    
--- src/archivematicaCommon/lib/externals/extractMaildirAttachments.py
+++ src/archivematicaCommon/lib/externals/extractMaildirAttachments.py
@@ -4,7 +4,6 @@
 #Author Ian Lewis
 #http://www.ianlewis.org/en/parsing-email-attachments-python
 
-import mailbox
 from email.Header import decode_header
 import email
 from base64 import b64decode
@@ -13,7 +12,6 @@ from email.Parser import Parser as EmailParser
 from email.utils import parseaddr
 # cStringIOはダメ
 from StringIO import StringIO
-import os
 
 class NotSupportedMailFormat(Exception):
     pass
@@ -35,7 +33,8 @@ def parse_attachment(message_part):
 
             for param in dispositions[1:]:
                 name,value = param.split("=")
-                name = name.lower().strip() 
+                name = name.lower()
+
                 if name == "filename":
                     attachment.name = value
                 elif name == "create-date":
@@ -95,44 +94,6 @@ def parse(content):
         'from' : parseaddr(msgobj.get('From'))[1], # 名前は除いてメールアドレスのみ抽出
         'to' : parseaddr(msgobj.get('To'))[1], # 名前は除いてメールアドレスのみ抽出
         'attachments': attachments,
-        'msgobj' : msgobj,
+        'msgobj': msgobj,
     }
-    
-if __name__ == '__main__':
-    #http://www.doughellmann.com/PyMOTW/mailbox/
-    maildir = sys.argv[1]
-    import lxml.etree as etree
-    #print "Extracting attachments from: " + maildir
-    root = etree.Element("ArchivematicaMaildirAttachmentExtractionRecord")
-    root.set("directory", maildir) 
-    for maildirsub2 in os.listdir(maildir):
-        maildirsub = os.path.join(maildir, maildirsub2)
-        #print "Extracting attachments from: " + maildirsub
-        md = mailbox.Maildir(maildirsub)
-        directory = etree.SubElement(root, "subDir")
-        directory.set("dir", maildirsub2)
-        for item in md.iterkeys():
-            #print maildirsub2, item
-            fil = md.get_file(item)
-            out = parse(fil)
-            #for i in out.iterkeys():
-                #print i
-            if len(out['attachments']):
-                msg = etree.SubElement(directory, "msg")
-                etree.SubElement(msg, "Subject").text = out["subject"] 
-                etree.SubElement(msg, "Date").text = out['msgobj']['date']
-                etree.SubElement(msg, "To").text = out["to"]
-                etree.SubElement(msg, "From").text = out["from"]
-                for i in range(len(out['attachments'])):
-                    attachment = out['attachments'][i]
-                    #attachment = StringIO(file_data) TODO LOG TO FILE
-                    attch = etree.SubElement(msg, "attachment")
-                    etree.SubElement(attch, "name").text = attachment.name
-                    etree.SubElement(attch, "content_type").text = attachment.content_type
-                    etree.SubElement(attch, "size").text = str(attachment.size)
-                    etree.SubElement(attch, "create_date").text = attachment.create_date
-                    etree.SubElement(attch, "mod_date").text = attachment.mod_date
-                    etree.SubElement(attch, "read_date").text = attachment.read_date
-    print etree.tostring(root, pretty_print=True)
-
                     
