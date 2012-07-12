1bc3e2ac0eda9ac32348774975f5856ebf78e80b
Mike Cantelon <mcantelon@gmail.com>
2012-07-12 17:48:54 +0000

    Remove delete icon from AIP browser.
    Autoconverted from SVN (revision:2889)

--- src/dashboard/src/media/js/ingest/aip_browser.js
+++ src/dashboard/src/media/js/ingest/aip_browser.js
@@ -1,32 +1,4 @@
 function setupAIPBrowser(directory) {
-  function moveHandler(move) {
-    // don't allow dragging stuff from originals directory?
-    if (move.self.id != 'originals') {
-      if (move.allowed) {
-        move.self.busy();
-
-        $.post(
-          '/filesystem/copy_to_arrange/',
-          {
-            filepath: move.droppedPath,
-            destination: move.containerPath
-          },
-          function(result) {
-            if (result.error == undefined) {
-              arrange.refresh(arrangeDirectory);
-            } else {
-              alert(result.message);
-              move.self.idle();
-            }
-          }
-        );
-      } else {
-        alert("You can't move a directory into its subdirectory.");
-      }
-    } else {
-      alert("You can't copy into the originals directory.");
-    }
-  }
 
   var explorer = new FileExplorer({
     el: $('#explorer'),
@@ -34,7 +6,7 @@ function setupAIPBrowser(directory) {
     entryTemplate: $('#template-dir-entry').html()
   });
 
-  explorer.moveHandler = moveHandler;
+  explorer.options.actionHandlers = [];
 
   explorer.refresh(directory);
 }
