040435e9e01dd41f7c073e37ef06de31346fd279
Joseph Perry <berwin22@gmail.com>
2012-07-17 21:49:32 +0000

    More verbose output on re-create db to isolate problems.
    Autoconverted from SVN (revision:2974)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -6,14 +6,41 @@ password="demo"
 echo "Removing existing units"
 sudo ./removeUnitsFromWatchedDirectories.py
 
-echo "Enter root database password for each the following. (Hit enter if blank)"
+set -e
+echo "Enter the DATABASE root password (Hit enter if blank)"
+read dbpassword
 
-mysql -u root -p --execute="DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;" \
-                 --execute="USE $databaseName; source $currentDir/../src/MCPServer/share/mysql;" \
-                 --execute="USE $databaseName; source $currentDir/../src/transcoder/share/mysql;" \
-                 --execute="DROP USER '${username}'@'localhost';" \
-                 --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';" \
-                 --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost';" 
+if [ ! -z "$dbpassword" ] ; then
+    dbpassword="-p${dbpassword}"
+else
+    dbpassword=""
+fi
+#set -o verbose #echo on
+pwd
+currentDir="`dirname $0`"
+set +e
+echo "Removing the old database"
+mysql -u root "${dbpassword}" --execute="DROP DATABASE IF EXISTS ${databaseName}"
+echo "Removing ${username} user"
+mysql -u root "${dbpassword}" --execute="DROP USER '${username}'@'localhost';"
+set -e
 
-#This command would auto add a user 'demo' with password 'demo'
+echo "Creating MCP database"
+mysql -u root "${dbpassword}" --execute="CREATE DATABASE ${databaseName} CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+
+echo "Creating and populating MCP Tables"
+mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/../src/MCPServer/share/mysql;"
+
+echo "Creating and populating Transcoder Tables"
+mysql -u root "${dbpassword}" --execute="USE ${databaseName}; SOURCE $currentDir/../src/transcoder/share/mysql;"
+
+echo "Creating ${username} user"
+mysql -u root "${dbpassword}" --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
+mysql -u root "${dbpassword}" --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost';"
+
+#echo "Creating dashboard user"
 #mysql -u root -p --execute="INSERT INTO auth_user (username, email, password, is_staff, is_active, is_superuser, date_joined) VALUES ('demo', 'demo@example.com', 'sha1$e7fc2$6123f456bba92c67a409baf2c282398fc5f70fc9', TRUE, TRUE, TRUE, NOW() );"
+
+#set +o verbose #echo off
+printGreen="Database created successfully."
+echo -e "\e[6;32m${printGreen}\e[0m"
