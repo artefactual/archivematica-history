a0dee1b9cc2face2d43af55cc9fa8bde5483a8e4
Joseph Perry <berwin22@gmail.com>
2012-07-17 18:54:38 +0000

    Adding command to auto add the dashboard user demo to the dev. The command is commented out, so it will not take effect.
    Autoconverted from SVN (revision:2967)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -13,4 +13,7 @@ mysql -u root -p --execute="DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHA
                  --execute="USE $databaseName; source $currentDir/../src/transcoder/share/mysql;" \
                  --execute="DROP USER '${username}'@'localhost';" \
                  --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';" \
-                 --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost';"
+                 --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost';" 
+
+#This command would auto add a user 'demo' with password 'demo'
+#mysql -u root -p --execute="INSERT INTO auth_user (username, email, password, is_staff, is_active, is_superuser, date_joined) VALUES ('demo', 'demo@example.com', 'sha1$e7fc2$6123f456bba92c67a409baf2c282398fc5f70fc9', TRUE, TRUE, TRUE, NOW() );"
