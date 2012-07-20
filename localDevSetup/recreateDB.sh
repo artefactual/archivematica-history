9df2190a0d9a4f8614d0f47952ed0bfe4132f12d
Joseph Perry <berwin22@gmail.com>
2012-07-20 22:23:26 +0000

    Dashboard user: demo/demo will automatically be created in dev.
    Autoconverted from SVN (revision:3004)

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -38,8 +38,10 @@ echo "Creating ${username} user"
 mysql -u root "${dbpassword}" --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
 mysql -u root "${dbpassword}" --execute="GRANT SELECT, UPDATE, INSERT, DELETE ON ${databaseName}.* TO '${username}'@'localhost';"
 
-#echo "Creating dashboard user"
-#mysql -u root -p --execute="INSERT INTO auth_user (username, email, password, is_staff, is_active, is_superuser, date_joined) VALUES ('demo', 'demo@example.com', 'sha1$e7fc2$6123f456bba92c67a409baf2c282398fc5f70fc9', TRUE, TRUE, TRUE, NOW() );"
+echo "Creating dashboard user"
+mysql -u root "${dbpassword}" --execute="USE ${databaseName}; INSERT INTO auth_user (username, email, password, is_staff, is_active, is_superuser, date_joined) VALUES ('demo', 'demo@example.com', 'sha1\$e7fc2\$6123f456bba92c67a409baf2c282398fc5f70fc9', TRUE, TRUE, TRUE, NOW() );"
+
+dbpassword=""
 
 #set +o verbose #echo off
 printGreen="${databaseName} database created successfully."
