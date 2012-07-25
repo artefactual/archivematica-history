26170011408dfa1657137d0634c36e75e43090d4
Mike Cantelon <mcantelon@gmail.com>
2012-07-25 22:25:58 +0000

    Simplified code.
    Autoconverted from SVN (revision:3041)

--- src/dashboard/src/media/js/administration/source_directory_picker.js
+++ src/dashboard/src/media/js/administration/source_directory_picker.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  $('#directories').hide();
-  $('#source_page_instructions').hide();
-
   var ajaxChildDataUrl = '/filesystem/children/'
     , picker = new DirectoryPickerView({
       el:               $('#explorer'),
@@ -9,22 +6,12 @@ $(document).ready(function() {
       entryTemplate:    $('#template-dir-entry').html(),
       ajaxChildDataUrl: ajaxChildDataUrl
   });
-  picker.updateSources(function() {
-    var picker = new DirectoryPickerView({
-      el: $('#explorer'),
-      levelTemplate: $('#template-dir-level').html(),
-      entryTemplate: $('#template-dir-entry').html(),
-      ajaxChildDataUrl: ajaxChildDataUrl
-    });
-    picker.busy();
-    picker.structure = {
-      'name': 'home',
-      'parent': '',
-      'children': []
-    };
-    picker.render();
-    picker.idle();
-    $('#directories').slideDown();
-    $('#source_page_instructions').fadeIn();
-  });
+
+  picker.structure = {
+    'name': 'home',
+    'parent': '',
+    'children': []
+  };
+
+  picker.render();
 });
